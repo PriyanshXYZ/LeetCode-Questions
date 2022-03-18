a7d7a8be2d436b88d36e4a0dd6a4270448a92c52
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-03-18 11:04:11 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (40.05%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,37 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int strt=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                strt=i;
-                break;
+    public int[] searchRange(int[] a, int target) {
+        if(a.length==0)return new int[]{-1,-1};
+        return new int[]{ firstIdxbs(a,target), lastIdxbs(a,target)};
+    }
+    public int firstIdxbs(int[] a,int target){
+        int i=0,j=a.length-1;
+        int idx=-1;
+        while(i<j-1){
+            int mid=i+(j-i)/2;
+            if(a[mid]>=target){
+                //update j drop i
+                j=mid;
+            }else{
+                i=mid;
             }
         }
-        int end=-1;
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]==target){
-                end=i;
-                break;
+        if(a[i]==target) return i;
+        if(a[j]==target) return j;
+        return idx;
+    }
+     public int lastIdxbs(int[] a,int target){
+        int i=0,j=a.length-1;
+        int idx=-1;
+        while(i<j-1){
+            int mid=i+(j-i)/2;
+            if(a[mid]<=target){
+                i=mid;
+            }else{
+               j=mid;
             }
         }
-        return new int[]{strt,end};
+        if(a[j]==target) return j;
+        if(a[i]==target) return i;
+        return idx;
     }
 }
