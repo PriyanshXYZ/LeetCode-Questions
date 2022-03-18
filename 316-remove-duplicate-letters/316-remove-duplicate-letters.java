3374454381811a1efad4c34255c37e4c4a7a42cc
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-03-18 23:18:37 +0530

    Time: 2 ms (98.35%), Space: 40.6 MB (81.17%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,6 +1,6 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] alphabetFreq=new int[26];
+         int[] alphabetFreq=new int[26];
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             alphabetFreq[ch-'a']++;
