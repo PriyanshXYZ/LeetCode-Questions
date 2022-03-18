ab84f57b068752d6aef862008639b4b71e013d42
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-03-18 23:39:28 +0530

    Time: 3 ms (82.62%), Space: 42.4 MB (25.28%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,30 +1,37 @@
 class Solution {
     public String smallestSubsequence(String s) {
-         int[] alphabetFreq=new int[26];
+        //make a freq array
+        int[] freq=new int[26];
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            alphabetFreq[ch-'a']++;
+            freq[ch-'a']++;
         }
-        boolean[] seen=new boolean[26];
+        //make a visited array which gets true if we add to stack
+        //make a stack
+        boolean[] visited=new boolean[26];
         Stack<Integer> stk=new Stack();
         
-       
         for(int i=0;i<s.length();i++){
             int curr=s.charAt(i)-'a';
-            alphabetFreq[curr]--;
-            if(seen[curr])continue;
-            while(!stk.isEmpty() && curr<stk.peek() && alphabetFreq[stk.peek()]>0){
+            //skip if character is already inserted in the stack
+            freq[curr]--;
+            if(visited[curr]) continue;
+            //pop the element when stack (not empty) when
+            //1) curr element is smaller than stack's top (lexicograpically smaller)
+            //2) there is element present in string after curr idx
+            
+            while(!stk.isEmpty() && curr<stk.peek() && freq[stk.peek()]>0){
                 int popped=stk.pop();
-                seen[popped]=false;
-                
+                visited[popped]=false;
             }
+            //if element cannot be popped then add to stack and do visited[curr]=true;
             stk.push(curr);
-            seen[curr]=true;
+            visited[curr]=true;
         }
-         StringBuilder sb=new StringBuilder();
+        StringBuilder sb=new StringBuilder();
         while(stk.size()>0){
             sb.append((char)(stk.pop()+'a'));
-        }
-        return sb.reverse().toString();
+        }   
+        return sb.reverse().toString(); 
     }
 }
