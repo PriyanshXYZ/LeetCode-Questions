778c42cfb0e1ff645ae85a9c77ed3ea73d4534f9
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-03-21 00:02:50 +0530

    Time: 1 ms (67.90%), Space: 43.1 MB (13.18%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,10 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            //to search if element is already present at mid left or right
+            if(nums[mid]==target){
+                return mid;
+            }        
+            if(nums[mid]<=nums[right]){
+                //mid to right is sorted
+                if(target>nums[mid] && target<=nums[right]) left=mid+1;
+                else right=mid-1;
+            }else{
+                if(target<nums[mid] && target>=nums[left]) right=mid-1;
+                else left=mid+1;
             }
         }
-        return -1;
+        return -1; 
+
     }
 }
