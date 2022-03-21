fd9698b0559aa5b6666b447fa54273980f5f07fa
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-03-21 22:59:17 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (10.10%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -21,6 +21,9 @@ class Solution {
     public boolean helper(TreeNode r1,TreeNode r2){
         if(r1==null && r2==null)return true;
         if(r1==null || r2==null)return false;
-        return (r1.val==r2.val) && helper(r1.left,r2.right) && helper(r1.right,r2.left);
+        if(r1.val!=r2.val)return false;
+        boolean ans1=helper(r1.left,r2.right);
+        boolean ans2=helper(r1.right,r2.left);
+        return ans1 && ans2;  
     }
 }
