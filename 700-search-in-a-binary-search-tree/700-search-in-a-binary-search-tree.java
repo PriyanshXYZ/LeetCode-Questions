fbd777e42a9d058014e644915968369f63518ba4
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-03-30 10:33:34 +0530

    Time: 1 ms (8.55%), Space: 52.1 MB (15.74%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -15,6 +15,9 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
+        return searchBSTusingBFS(root,val);
+    }
+    public TreeNode searchBSTusingDFS(TreeNode root,int val){
         if(root==null) return null;
         if(root.val==val)return root;
         else if(root.val>val){
@@ -23,6 +26,25 @@ class Solution {
         }else{
             TreeNode rch=searchBST(root.right,val);
             if(rch!=null)return rch;
+        }
+        return null;
+    }
+    public TreeNode searchBSTusingBFS(TreeNode root,int val){
+        if(root==null)return null;
+        Queue<TreeNode> q=new ArrayDeque();
+        q.add(root);
+        while(q.size()>0){
+            int size=q.size();
+            while(size-->0){
+                TreeNode rnode=q.remove();
+                if(rnode.val==val){
+                    return rnode;
+                }else if(rnode.val>val && rnode.left!=null){
+                    q.add(rnode.left);
+                }else if(rnode.val<val && rnode.right!=null){
+                    q.add(rnode.right);
+                }
+            }
         }
         return null;
     }
