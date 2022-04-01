1dc59599f669caa08f92472a366569ca1418f9e8
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-04-02 01:48:32 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (22.67%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int min(TreeNode root){
+        if(root==null)return Integer.MAX_VALUE;
+        return Math.min(min(root.left),root.val);
+    }
+    public int max(TreeNode node){
+        if(node==null) return Integer.MIN_VALUE;
+        return Math.max(max(node.right),node.val);
+    }
+    public TreeNode deleteNode(TreeNode node, int data) {
+        if(node==null)return null;
+        if(node.val>data){
+          node.left=deleteNode(node.left,data);
+          return node;
+        }else if(node.val<data){
+          node.right=deleteNode(node.right,data);
+          return node;
+        }else{
+          //case1
+          if(node.left==null && node.right==null){
+            return null;
+          }
+          //case2
+          if(node.left==null){
+            return node.right;
+          }
+          if(node.right==null){
+            return node.left;
+          }
+          //case3
+          int justSmaller=max(node.left);
+          node.val=justSmaller;
+          node.left=deleteNode(node.left,justSmaller);
+          return node;
+        }
+    
+  }
+    public int kthSmallest(TreeNode root, int k) {
+        if(root==null)return Integer.MAX_VALUE;
+        while(k-->1){
+            root=deleteNode(root,min(root));
+        }
+        return min(root);
+    }
+}
