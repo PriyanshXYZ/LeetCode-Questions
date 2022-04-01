b241fad5d6cb46d1cd2154c8c801305b9cf77890
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-04-02 02:04:21 +0530

    Time: 1 ms (70.98%), Space: 43.3 MB (81.38%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -13,48 +13,62 @@
  *     }
  * }
  */
-class Solution {
-    public int min(TreeNode root){
+/*
+public int min(TreeNode root){
         if(root==null)return Integer.MAX_VALUE;
         return Math.min(min(root.left),root.val);
     }
-    public int max(TreeNode node){
-        if(node==null) return Integer.MIN_VALUE;
-        return Math.max(max(node.right),node.val);
+    public int max(TreeNode root){
+        if(root==null) return Integer.MIN_VALUE;
+        return Math.max(max(root.right),root.val);
     }
-    public TreeNode deleteNode(TreeNode node, int data) {
-        if(node==null)return null;
-        if(node.val>data){
-          node.left=deleteNode(node.left,data);
-          return node;
-        }else if(node.val<data){
-          node.right=deleteNode(node.right,data);
-          return node;
+    public TreeNode deleteNode(TreeNode root, int val) {
+        if(root==null)return null;
+        if(root.val>val){
+          root.left=deleteNode(root.left,data);
+          return root;
+        }else if(root.val<data){
+          root.right=deleteNode(root.right,data);
+          return root;
         }else{
           //case1
-          if(node.left==null && node.right==null){
+          if(root.left==null && root.right==null){
             return null;
           }
           //case2
-          if(node.left==null){
-            return node.right;
+          if(root.left==null){
+            return root.right;
           }
-          if(node.right==null){
-            return node.left;
+          if(root.right==null){
+            return root.left;
           }
           //case3
-          int justSmaller=max(node.left);
-          node.val=justSmaller;
-          node.left=deleteNode(node.left,justSmaller);
-          return node;
+          int justSmaller=max(root.left);
+          root.val=justSmaller;
+          root.left=deleteNode(root.left,justSmaller);
+          return root;
         }
     
   }
     public int kthSmallest(TreeNode root, int k) {
         if(root==null)return Integer.MAX_VALUE;
-        while(k-->1){
+        while(k-->1){//delete elements only when we need to find 2nd smallet or larger
             root=deleteNode(root,min(root));
         }
         return min(root);
+    }
+    */
+class Solution {
+    List<Integer> ans=new ArrayList();
+    public List<Integer> dfs(TreeNode root){
+        if(root==null)return new ArrayList();
+        List<Integer> left=dfs(root.left);
+        ans.add(root.val);
+        List<Integer> right=dfs(root.right);
+        return ans;
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> res=dfs(root);
+        return res.get(k-1);
     }
 }
