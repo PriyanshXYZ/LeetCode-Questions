45016ee27d0dbf848f5df9792c2e06b65837fcd4
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-04-14 01:28:10 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (81.70%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -5,37 +5,41 @@ class Solution {
         int maxRow=m-1,maxCol=n-1;
         int minRow=0,minCol=0;
         
-        int count=1;
+        int count=0;
         while(count<=m*n){
             //rightward
-            for(int col=minCol;col<=maxCol && count<=m*n;col++){
+            for(int col=minCol;col<=maxCol;col++){
                 int row=minRow;
                 ans.add(mat[row][col]);
                 count++;
+                if(count==m*n)return ans;
             }
             minRow=minRow+1;
             
             //downward
-            for(int row=minRow;row<=maxRow && count<=m*n;row++){
+            for(int row=minRow;row<=maxRow;row++){
                 int col=maxCol;
                 ans.add(mat[row][col]);
                 count++;
+                if(count==m*n)return ans;
             }
             maxCol=maxCol-1;
             
             //leftward
-            for(int col=maxCol;col>=minCol && count<=m*n;col--){
+            for(int col=maxCol;col>=minCol;col--){
                 int row=maxRow;
                 ans.add(mat[row][col]);
                 count++;
+                if(count==m*n)return ans;
             }
             maxRow=maxRow-1;
             
             //upwards
-            for(int row=maxRow;row>=minRow && count<=m*n;row--){
+            for(int row=maxRow;row>=minRow;row--){
                 int col=minCol;
                 ans.add(mat[row][col]);
                 count++;
+                if(count==m*n)return ans;
             }
             minCol=minCol+1;
         }
