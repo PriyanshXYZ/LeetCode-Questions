bd355a73f16f90064419c93bb4d9748f1564777b
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-04-21 22:32:52 +0530

    Time: 57 ms (30.69%), Space: 49.4 MB (52.40%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,7 +2,26 @@ class Solution {
     public int jump(int[] nums) {
         int[] dp=new int[nums.length];
         // return recursion(0,nums.length-1,nums);
-        return memoization(0,nums.length-1,nums,dp);
+        // return memoization(0,nums.length-1,nums,dp);
+        int n=nums.length-1;
+        //tabulation
+        for(int idx=n;idx>=0;idx--){
+            if(idx==n){
+                dp[idx]=0;
+                continue;
+            } 
+            
+            int ans=Integer.MAX_VALUE;
+            for(int jumps=1; jumps <= nums[idx] ;jumps++){
+                if(idx+jumps<=n)
+                    ans=Math.min(dp[idx+jumps],ans);
+            }
+            if(ans!=Integer.MAX_VALUE){
+                ans+=1;
+            }
+            dp[idx]=ans;    
+        }
+        return dp[0];
     }
     public int recursion(int idx,int n,int[] nums){
         if(idx==n)return 0;
