72063d7774f1b4c019cbbe777c25d75b1e54a5cd
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-03 00:56:01 +0530

    Time: 2 ms (38.15%), Space: 48.3 MB (51.15%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -4,11 +4,32 @@ class Solution {
         a[i]=a[j];
         a[j]=temp;
     }
+    // public int[] sortArrayByParity(int[] a) {
+    //     for(int i=0;i<a.length;i++){
+    //         for(int j=i+1;j<a.length;j++){
+    //             if(a[i]%2!=0 && a[j]%2==0){
+    //                 swap(i,j,a);
+    //             }
+    //         }
+    //     }
+    //     return a;
+    // }
     public int[] sortArrayByParity(int[] a) {
-        for(int i=0;i<a.length;i++){
-            for(int j=i+1;j<a.length;j++){
-                if(a[i]%2!=0 && a[j]%2==0){
-                    swap(i,j,a);
+        int left=0,right=a.length-1;
+        while(left<right){
+            //left even 
+            if(a[left]%2==0){
+                //right odd
+                left++;
+            }//left odd
+            else{
+                //right even
+                if(a[right]%2==0){
+                    swap(left,right,a);
+                    left++;
+                    right--;
+                }else{
+                    right--;
                 }
             }
         }
