acf3d4dff4f621ffe657449ddb19314d1a3a9f25
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-10 00:06:54 +0530

    Time: 1 ms (91.35%), Space: 42.3 MB (71.64%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -2,13 +2,13 @@ class Solution {
     String[] numpad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
         if(digits.length()<1)return new ArrayList();
-        return recursion(0,digits,""); 
+        return recursion(0,digits,new StringBuilder()); 
     }
     
-    public List<String> recursion(int idx,String digits,String output){
+    public List<String> recursion(int idx,String digits,StringBuilder output){
         if(idx==digits.length()){
             List<String> base=new ArrayList();
-            base.add(output);
+            base.add(output.toString());
             return base;
         }
         if(idx>digits.length()){
@@ -19,7 +19,8 @@ class Solution {
         char a=digits.charAt(idx);
         for(int i=0;i<numpad[a-'0'].length();i++){
             char ch=numpad[a-'0'].charAt(i);
-            ans.addAll(recursion(idx+1,digits,output+ch));
+            ans.addAll(recursion(idx+1,digits,output.append(ch)));
+            output.deleteCharAt(output.length()-1);
         }
         
         return ans;
