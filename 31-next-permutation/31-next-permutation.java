73a8e570d96712e8f4e944955f0fb041ea4303e0
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-12 19:15:54 +0530

    Time: 1 ms (82.99%), Space: 43.7 MB (51.57%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,4 +1,18 @@
 class Solution {
+     // 1. Find the largest index k such that nums[k] < nums[k + 1]. If no such index , just reverse
+    // 2. Find the largest index l > k such that nums[k] < nums[l]
+    // 3. Swap nums[k] and nums[l]
+    // 4. Reverse the sub-array nums[k + 1:]
+    
+    // how to understand it:
+    // step-1: easy, find the first digit that can be swapped to make permutation bigger
+    // step-2: easy, find the digit bigger but closest to nums[k]
+    // step-3: swap(nums[k], nums[l])
+    // step-4: sort the subarray nums[k+1:end], why we can just reverse instead of sort?
+    //         because we know nums[k+1:end] must be non-increasing, reason:
+    //         1. at step 1, we know nums[k+1:end] is non-decreasing
+    //         2. before swap in step 3, we know nums[l-1] >= nums[l] > nums[k] >= nums[l+1]
+    //         3. so after swap, we still have nums[l-1] > nums[k] >= nums[l+1], so we can reverse it
     public void nextPermutation(int[] nums) {
         //corner case
         if(nums.length<2)return;
