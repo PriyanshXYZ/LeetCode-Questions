44eae716fc0d98626a197b717668949122521d86
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-18 18:03:08 +0530

    Added solution - LeetHub

--- Minimum Number in a sorted rotated array - GFG/minimum-number-in-a-sorted-rotated-array.java
+++ Minimum Number in a sorted rotated array - GFG/minimum-number-in-a-sorted-rotated-array.java
@@ -37,19 +37,12 @@ class Solution
         // Your code here
         while(low<high){
             int mid=low+(high-low)/2;
-            if(arr[low]<arr[high]){
-                if(arr[mid]<arr[high]){
-                    high=mid;
-                }else{
-                    low=mid+1;
-                }
+            if(arr[mid]<arr[high]){
+                high=mid;
             }else{
-                if(arr[mid]<arr[high]){
-                    high=mid;
-                }else{
-                    low=mid+1;
-                }
+                low=mid+1;
             }
+            
         }
         return arr[low];
     }
