1765704bfaadbfc309896714548d166a65b12ab7
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-18 22:59:04 +0530

    Time: 1 ms (26.70%), Space: 41.7 MB (61.93%) - LeetHub

---
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int xleftSubTreeSum=0;
+    int xrightSubTreeSum=0;
+    public int size(TreeNode root,int x){
+        if(root==null)return 0;
+        
+        int lch=size(root.left,x);
+        int rch=size(root.right,x);
+        //if the root is x we get left and right no of nodes sum
+        if(root.val==x){
+            xleftSubTreeSum=lch;
+            xrightSubTreeSum=rch;
+        }
+        return lch+rch+1;
+    }
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        size(root,x);
+        
+        //now we have ancestor subtree sum of left and right
+        int otherSide=n-(xleftSubTreeSum + xrightSubTreeSum+1);
+        
+        //finding max of all subtree possible
+        int max=Math.max(otherSide,Math.max(xrightSubTreeSum,xleftSubTreeSum));
+        
+        int rest=n-max;
+        
+        if(rest<max){
+            return true ;
+        }
+        return false;
+    }
+}
