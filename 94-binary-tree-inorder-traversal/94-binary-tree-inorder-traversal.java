79f28ee9196fa451f3c9c0d91ec56bc58d13cef6
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-19 22:26:37 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (45.40%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,32 +15,56 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        return morrisTraversal(root);
+    }
+    public List<Integer> morrisTraversal(TreeNode root){
+        if(root==null)return new ArrayList();
         List<Integer> ans=new ArrayList();
+        TreeNode curr=root;
         
-        helper(root,ans);
+        while(curr!=null){
+            if(curr.left==null){
+                ans.add(curr.val);
+                curr=curr.right;
+            }else{
+                //check if inorderPredecessor is null or equal to curr node
+                TreeNode inorderPred=curr.left;
+                while(inorderPred.right!=null && inorderPred.right!=curr){
+                    inorderPred=inorderPred.right;
+                }
+                //if inorderPredecessor is null meaning we have not visited left subtree
+                if(inorderPred.right==null){
+                    inorderPred.right=curr;//making of thread/link to inorderSuccessor
+                    curr=curr.left;
+                }else{
+                    ans.add(curr.val);
+                    inorderPred.right=null;
+                    curr=curr.right;
+                }
+            }
+        }
         return ans;
     }
-    public void helper(TreeNode root,List<Integer> a){
+    public void dfs(TreeNode root,List<Integer> a){
         if(root==null) return;
-        helper(root.left,a);
+        dfs(root.left,a);
         a.add(root.val);
-        helper(root.right,a);
+        dfs(root.right,a);
     }
-    // //using bfs
-    // public List<Integer> inorderTraversal(TreeNode root) {
-    //     if(root==null) return new ArrayList();
-    //     List<Integer> a=new ArrayList();
-    //     Stack<TreeNode> stk=new Stack();
-    //     TreeNode curr=root;
-    //     while(curr!=null || stk.size()>0){
-    //         while(curr!=null){
-    //             stk.push(curr);
-    //             curr=curr.left;
-    //         }
-    //         curr=stk.pop();
-    //         a.add(curr.val);
-    //         curr=curr.right;
-    //     }
-    //     return a;
-    // }
+    public List<Integer> Bfs(TreeNode root) {
+        if(root==null) return new ArrayList();
+        List<Integer> a=new ArrayList();
+        Stack<TreeNode> stk=new Stack();
+        TreeNode curr=root;
+        while(curr!=null || stk.size()>0){
+            while(curr!=null){
+                stk.push(curr);
+                curr=curr.left;
+            }
+            curr=stk.pop();
+            a.add(curr.val);
+            curr=curr.right;
+        }
+        return a;
+    }
 }
