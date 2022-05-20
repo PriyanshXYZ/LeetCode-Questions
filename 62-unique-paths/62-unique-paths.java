2edb9ef498d7b0e80b0cb99613a591926b46ec91
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-20 16:02:56 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (58.57%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        boolean[][] visited=new boolean[m][n];
+        int[][] dp=new int[m][n];
+        return memo(m-1,n-1,visited,dp);
+    }
+    public int recursion(int m,int n,boolean[][] visited){
+        if(m==0 &&n==0)return 1;
+        if(m<0 || n<0)return 0;
+        int ans=0;
+        visited[m][n]=true;
+        if(m>=1 && !visited[m-1][n]){
+            ans+=recursion(m-1,n,visited);    
+        }
+        if(n>=1 && !visited[m][n-1]){
+            ans+=recursion(m,n-1,visited);
+        }
+        visited[m][n]=false;
+        
+        return ans;
+    }
+    public int memo(int m,int n,boolean[][] visited,int[][] dp){
+        if(m==0 &&n==0)return dp[m][n]=1;
+        if(m<0 || n<0)return dp[m][n]=0;
+        if(dp[m][n]!=0)return dp[m][n];
+        int ans=0;
+        visited[m][n]=true;
+        if(m>=1 && !visited[m-1][n]){
+            ans+=memo(m-1,n,visited,dp);    
+        }
+        if(n>=1 && !visited[m][n-1]){
+            ans+=memo(m,n-1,visited,dp);
+        }
+        visited[m][n]=false;
+        
+        return dp[m][n]=ans;
+    }
+}
