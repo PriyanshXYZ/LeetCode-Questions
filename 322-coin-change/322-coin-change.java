72a883f370ea2d733b329a44dcc237418df1db67
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-23 00:35:07 +0530

    Time: 61 ms (16.78%), Space: 72.1 MB (7.70%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,8 +2,9 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         // int ans=recursion(coins.length-1,amount,coins);
         int[][] dp=new int[coins.length][amount+1];
-        int ans=memo(coins.length-1,amount,coins,dp);
-        return ans<Integer.MAX_VALUE-1?ans:-1;
+        // int ans=memo(coins.length-1,amount,coins,dp);
+        return tabulation(coins,amount);
+        // return ans<Integer.MAX_VALUE-1?ans:-1;
     }
     public int recursion(int n,int target,int[] coins){
         if(target==0)return 0;
@@ -22,6 +23,30 @@ class Solution {
         inc=memo(n,target-coins[n],coins,dp)+1;//path is included so add this to ans
         exc=memo(n-1,target,coins,dp);
         return dp[n][target]=Math.min(inc,exc);
+    }
+    
+    public int tabulation(int[] coins, int amount){
+        int[][] dp=new int[coins.length][amount+1];
+        int n=coins.length;
+        for(int idx=0;idx<n;idx++){
+            for(int amt=0;amt<=amount;amt++){
+                if(amt==0){
+                    dp[idx][amt]=0;
+                    continue;
+                }
+                
+                int inc=Integer.MAX_VALUE-1;
+                if(amt-coins[idx]>=0)
+                    inc=dp[idx][amt-coins[idx]]+1;
+                
+                int exc=Integer.MAX_VALUE-1;
+                if(idx>=1)
+                    exc=dp[idx-1][amt];
+                
+                dp[idx][amt]=Math.min(inc,exc);
+            }
+        }
+         return dp[n-1][amount]<Integer.MAX_VALUE-1?dp[n-1][amount]:-1;
     }
     
 }
