498df709c31997cf4e306f466a64359594806d9b
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-25 00:01:19 +0530

    Time: 1 ms (93.18%), Space: 47.4 MB (62.45%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -17,8 +17,26 @@ class Solution {
     int maxSum;
     public int maxPathSum(TreeNode root) {
         maxSum=Integer.MIN_VALUE;
-        dfs(root);
+        dfs2(root);
         return maxSum;
+    }
+    public int dfs2(TreeNode root){
+        //base case
+        if(root==null)return 0;
+        int left=dfs2(root.left);
+        int right=dfs2(root.right);
+        
+        int left_dash=Math.max(0,left);//handling negative value
+        int right_dash=Math.max(0,right);//handling negative value
+        
+        int currMaxIncRoot=Math.max(left_dash,right_dash)+root.val;
+            
+        int currN2N=left_dash+right_dash+root.val;
+        
+        maxSum=Math.max(maxSum,currN2N);
+        
+        
+        return currMaxIncRoot;
     }
     public int dfs(TreeNode root){
         if(root==null)return 0; //no gain in ans 
