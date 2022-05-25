e649d9b886cbff1fbdefaada77ca77ad922bb570
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-26 03:01:13 +0530

    Time: 24 ms (33.08%), Space: 41.6 MB (96.78%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -16,14 +16,32 @@
 class Solution {
     int count=0;
     public int pathSum(TreeNode root, int targetSum) {
-        if(root==null)return 0;
-        HashMap<Integer,Integer> map=new HashMap();//(prefixSum,frequency)
-        map.put(0,1);//here we need to add this because there is a possibility of getting 0 in pSum
-        dfs(root,root.val,map,targetSum);
+        bruteForce(root,targetSum);
+        // if(root==null)return 0;
+        // HashMap<Integer,Integer> map=new HashMap();//(prefixSum,frequency)
+        // map.put(0,1);//here we need to add this because there is a possibility of getting 0 in pSum
+        // dfs(root,root.val,map,targetSum);
         return count;
     }
     //using O(n^2) appproach
-    
+    void bruteForce(TreeNode root,int target){
+        if(root==null)return;
+        int ans =everyNodeAsRoot(root,target,0);
+        
+        bruteForce(root.left,target);
+        bruteForce(root.right,target);
+        
+    }
+    int everyNodeAsRoot(TreeNode root,int target,int sum){
+        if(root==null)return Integer.MIN_VALUE;
+        if(sum+root.val==target){
+            count++;
+        }
+        
+        int left=everyNodeAsRoot(root.left,target,sum+root.val);
+        int right=everyNodeAsRoot(root.right,target,sum+root.val);
+        return left+right;
+    }
     //using O(n) approach
     public void dfs(TreeNode root,int pSum,HashMap<Integer,Integer> map,int target){
         if(map.containsKey(pSum-target)){
