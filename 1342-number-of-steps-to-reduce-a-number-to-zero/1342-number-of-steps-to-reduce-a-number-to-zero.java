b4658efcf50c665a27c95bf8aaad643ab9f76abe
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-28 00:21:28 +0530

    Time: 1 ms (54.47%), Space: 40.6 MB (66.99%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,4 +1,20 @@
-class Solution {
+
+class Solution{
+    //TC O(1)
+     public int numberOfSteps(int num) {
+         if(num==0)return 0;
+         int res=0;
+         while(num!=0){
+             res+=(num&1)==1?2:1;
+             num=num>>1;
+         }
+         return res-1;
+     }
+}
+
+
+class Solution1 {
+    //time complexity O(n);
     public int numberOfSteps(int num) {
         if(num==0)return 0;
         int count=0;
@@ -12,6 +28,6 @@ class Solution {
             }
         }
         return count+1;
-
     }
+
 }
