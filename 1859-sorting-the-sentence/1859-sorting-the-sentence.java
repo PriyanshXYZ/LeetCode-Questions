760ce3837035e16447873b17f9f7787776de8e22
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-28 17:09:23 +0530

    Time: 3 ms (38.07%), Space: 42.1 MB (42.49%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words=s.split(" ");
+        HashMap<Integer,String> map=new HashMap();
+        for(int i=0;i<words.length;i++){
+            map.put(words[i].charAt(words[i].length()-1)-'0',words[i]);
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int pos:map.keySet()){
+            sb.append(map.get(pos).substring(0,map.get(pos).length()-1));
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
