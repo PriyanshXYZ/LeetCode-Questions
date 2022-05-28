80c7b835f9bcfa897358140e9ff6e57e9653d276
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-28 19:34:15 +0530

    Time: 3 ms (38.07%), Space: 42.1 MB (42.49%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0;i<words.length;i++){
             map.put(words[i].charAt(words[i].length()-1)-'0',words[i]);
         }
+        System.out.println(map);
         StringBuilder sb=new StringBuilder();
         for(int pos:map.keySet()){
             sb.append(map.get(pos).substring(0,map.get(pos).length()-1));
