a6b80e0c1cac086703fdc09b50ee671a471df91d
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-05-29 17:11:04 +0530

    Time: 1 ms (52.25%), Space: 43.9 MB (44.29%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -29,3 +29,27 @@ class Solution {
         return left && right;
     }
 }
+class Solution1 {
+    class Pair{
+        boolean isBst=true;
+        long min=Long.MAX_VALUE;
+        long max=Long.MIN_VALUE;
+    }
+    public boolean isValidBST(TreeNode root) {
+        Pair node=dfs(root);
+        return node.isBst;
+    }
+    public Pair dfs(TreeNode root){
+        if(root==null)return new Pair();
+        
+        Pair lp=dfs(root.left);
+        Pair rp=dfs(root.right);
+        
+        Pair self=new Pair();
+        self.isBst=lp.isBst && rp.isBst && lp.max<root.val && rp.min>root.val;
+        self.min=Math.max(root.val,lp.min);
+        self.max=Math.max(root.val,rp.max);
+        return self;
+    }
+    
+}
