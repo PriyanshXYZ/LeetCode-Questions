4a4f2d3fd84132b4e8991aac92f83243374da181
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-01 00:02:42 +0530

    Time: 0 ms (100.00%), Space: 50.1 MB (22.29%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -14,36 +14,41 @@
  * }
  */
 class Solution {
-    public int max(TreeNode node){
-        if(node==null) return Integer.MIN_VALUE;
-        return Math.max(max(node.right),node.val);
+    public int max(TreeNode root){
+        if(root==null)return Integer.MIN_VALUE;
+        
+        return Math.max(max(root.right),root.val);
     }
-    public TreeNode deleteNode(TreeNode node, int data) {
-    if(node==null)return null;
-    if(node.val>data){
-      node.left=deleteNode(node.left,data);
-      return node;
-    }else if(node.val<data){
-      node.right=deleteNode(node.right,data);
-      return node;
-    }else{
-      //case1
-      if(node.left==null && node.right==null){
-        return null;
-      }
-      //case2
-      if(node.left==null){
-        return node.right;
-      }
-      if(node.right==null){
-        return node.left;
-      }
-      //case3
-      int justSmaller=max(node.left);
-      node.val=justSmaller;
-      node.left=deleteNode(node.left,justSmaller);
-      return node;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        //base case
+        if(root==null)return null;
+        if(key<root.val){
+            root.left=deleteNode(root.left,key);
+            return root;
+        }else if(key>root.val){
+            root.right=deleteNode(root.right,key);
+            return root;
+        }else{
+            //when we found the key
+            //there are 3 possible cases
+            //1 : key is leaf
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            
+            //2 : key has a single child
+            if(root.left==null){
+                return root.right;
+            }
+            if(root.right==null){
+                return root.left;
+            }
+            //3 : key has both child
+            //this can be done by finding justSmaller or justLarger of the subtree and replace the values of it
+            int justSmaller=max(root.left);
+            root.val=justSmaller;
+            root.left=deleteNode(root.left,justSmaller);
+            return root;
+        }
     }
-    
-  }
 }
