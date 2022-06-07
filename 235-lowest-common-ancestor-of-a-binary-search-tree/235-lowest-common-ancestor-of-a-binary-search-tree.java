9e29512e8c0c74e5b60386d4a6aed7578ebb1141
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-07 23:01:39 +0530

    Time: 8 ms (36.91%), Space: 49.8 MB (47.98%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -11,13 +11,24 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null)return null;
-        
-        if(root.val<p.val && root.val<q.val){
-            return lowestCommonAncestor(root.right,p,q);
-        }else if(root.val>p.val && root.val>q.val){
+        //using log(N) time complexity
+        if(root.val>p.val && root.val>q.val){
+            //this means that  lowest common node may exist in left of a BST 
             return lowestCommonAncestor(root.left,p,q);
+        }else if(root.val< p.val && root.val< q.val){
+            //this means that lowest common node may exist in right of a BST 
+            return lowestCommonAncestor(root.right,p,q);
         }else{
-            return root;
+            /*this will handle the following cases
+            CASE 1
+            when root.val>p.val && root.val<q.val => which means current node is the required               lowest common ancestor(due to properties of BST)
+            
+            CASE 2
+            when root.val==p.val || root.val==q.val =>which means that one of the node is lowest             common ancestor of other node
+            
+            */
+            
+            return root; 
         }
     }
 }
