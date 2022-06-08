663b8bf1789b21d3d3c16af59ca925cc5a6c7937
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-08 13:56:51 +0530

    Time: 11 ms (47.03%), Space: 95.7 MB (16.53%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        return dfs(root);
+    }
+    public int bfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        Queue<TreeNode> q=new ArrayDeque();
+        int level=1;
+        q.add(root);
+        while(q.size()>0){
+            int size=q.size();
+            while(size-->0){
+                TreeNode rnode=q.remove();
+                if(rnode.left==null && rnode.right==null)return level;//leaf node
+                if(rnode.left!=null)q.add(rnode.left);
+                if(rnode.right!=null)q.add(rnode.right);
+            }
+            level++;
+        }
+        return level;
+    }
+    public int dfs(TreeNode root){
+        if(root==null)return 0;
+        if(root.left==null && root.right==null)return 1;
+        
+        if(root.left==null)return dfs(root.right)+1;
+        
+        if(root.right==null)return dfs(root.left)+1;
+        
+        int left=dfs(root.left);
+        int right=dfs(root.right);
+        
+        return Math.min(left,right)+1;
+    }
+}
