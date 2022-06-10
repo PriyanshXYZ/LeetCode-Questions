bc4404e4b0ca07d0e3329483fa5c1dc3488c0169
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-10 15:37:53 +0530

    Time: 1 ms (75.76%), Space: 44.3 MB (54.46%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+class Solution1 {
     public TreeNode lcaDeepestLeaves(TreeNode root) {
         int maxDepth=depth(root);
         TreeNode lca=lcaAtDepth(root,maxDepth);
@@ -35,16 +35,40 @@ class Solution {
         
         if(depth==1){
             return root;
-        }else if(lch!=null && rch!=null){
+        }else if(lch!=null && rch!=null){//left and right both subtree has deepest leaf present
             return root;
-        }else if(lch!=null){
+        }else if(lch!=null){//left subtree has deepest leaf present
             return lch;
-        }else if(rch!=null){
+        }else if(rch!=null){//right subtree has deepest leaf present
             return rch;
         }
-        else{
+        else{//if subtree has no depth==deepest nodes
             return null;
         }
         
+    }
+}
+class Solution {
+    TreeNode lca;
+    int maxDepth;
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        lca=null;
+        maxDepth=0;
+        depth(root,0);
+        return lca;
+    }
+    public int depth(TreeNode root,int depth){
+        maxDepth = Math.max(depth,maxDepth);
+        if(root==null){
+            return depth;
+        }
+        
+        int lchDepth=depth(root.left,depth+1);
+        int rchDepth=depth(root.right,depth+1);
+        
+        if(lchDepth==maxDepth && rchDepth==maxDepth){//we got the deepest leafs
+            lca=root;
+        }
+        return Math.max(lchDepth,rchDepth);
     }
 }
