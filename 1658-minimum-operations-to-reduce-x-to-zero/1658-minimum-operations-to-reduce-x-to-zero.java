8fa9788c895d74a990225d6b752591b424f39d6e
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-12 00:40:40 +0530

    Time: 5 ms (95.37%), Space: 95.2 MB (67.36%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution1 {
+    //O(n) TC and SC
     public int minOperations(int[] nums, int x) {
         HashMap<Integer,Integer> map=new HashMap();
         //get a subarray of max length (to get min of operations to reduce to 0)
@@ -19,5 +20,30 @@ class Solution {
             map.put(sum,i);
         }
         return size==0?-1:nums.length-size;
+    }
+}
+class Solution{
+    public int minOperations(int[] nums, int x) {
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        
+        if(sum-x==0)return nums.length;
+        
+        int left=0;
+        int minOp=Integer.MAX_VALUE;
+        int n=nums.length-1;
+        for(int right=0;right<=n;right++){
+            sum-=nums[right];
+            while(sum<x && left<=right){
+                sum+=nums[left];
+                left++;
+            }
+            if(sum==x){
+                minOp=Math.min(minOp,n-right+left);
+            }
+        }
+        return minOp==Integer.MAX_VALUE?-1:minOp;
     }
 }
