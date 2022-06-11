2d702104bb40d305efa8230e17da550a533a8094
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-11 12:21:01 +0530

    Time: 2 ms (99.39%), Space: 42 MB (99.68%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,31 +1,26 @@
 class Solution {
-    int[][] grid;
-    boolean[][] visited;
     public int maxAreaOfIsland(int[][] grid) {
-        this.grid=grid;
         int dr=grid.length,dc=grid[0].length;
         int currArea=0;
-        visited=new boolean[dr][dc];
         for(int sr=0;sr<dr;sr++){
             for(int sc=0;sc<dc;sc++){
                 if(grid[sr][sc]==1){
-                    currArea=Math.max(helper(grid,sr,sc,dr,dc,visited),currArea);
+                    currArea=Math.max(helper(grid,sr,sc,dr,dc),currArea);
                 }
-                visited[sr][sc]=true;
             }
         }
         return currArea;
     }
-    public int helper(int[][] grid,int sr,int sc,int dr,int dc,boolean[][] visited){
+    public int helper(int[][] grid,int sr,int sc,int dr,int dc){
         //base case
-        if(sr<0 || sr>=dr || sc<0 || sc>=dc || visited[sr][sc] || grid[sr][sc]!=1) return 0;
+        if(sr<0 || sr>=dr || sc<0 || sc>=dc || grid[sr][sc]!=1) return 0;
         int area=0;        
         //faith 
-        visited[sr][sc]=true;
-        area+=helper(grid,sr-1,sc,dr,dc,visited);//up
-        area+=helper(grid,sr,sc-1,dr,dc,visited);//left
-        area+=helper(grid,sr+1,sc,dr,dc,visited);//down
-        area+=helper(grid,sr,sc+1,dr,dc,visited);//right
+        grid[sr][sc]=0;
+        area+=helper(grid,sr-1,sc,dr,dc);//up
+        area+=helper(grid,sr,sc-1,dr,dc);//left
+        area+=helper(grid,sr+1,sc,dr,dc);//down
+        area+=helper(grid,sr,sc+1,dr,dc);//right
         return area+1;
     }
 }
