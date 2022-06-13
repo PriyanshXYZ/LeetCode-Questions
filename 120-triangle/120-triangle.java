9000f5a91f4efaa9baab5394639d34c744f293b9
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-13 14:33:37 +0530

    Time: 4 ms (61.51%), Space: 44.2 MB (41.06%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,9 +1,10 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp=new int[triangle.size()+1][triangle.get(triangle.size()-1).size()+1];
-        for(int r=0;r<triangle.size();r++)
-            Arrays.fill(dp[r],Integer.MIN_VALUE);
-        return memo(triangle,0,0,dp);
+        return tabulation(triangle);
+        // int[][] dp=new int[triangle.size()+1][triangle.get(triangle.size()-1).size()+1];
+        // for(int r=0;r<triangle.size();r++)
+        //     Arrays.fill(dp[r],Integer.MIN_VALUE);
+        // return memo(triangle,0,0,dp);
         // return recursion(triangle,0,0);
     }
     private int min(int ...args){
@@ -13,6 +14,7 @@ class Solution {
         }
         return min;
     }
+    //O(2^(row)) Tc and O(row) Sc
     public int recursion(List<List<Integer>> a,int row,int col){
         if(row==a.size())return 0;
         if(col>=a.get(row).size())return Integer.MAX_VALUE;
@@ -25,7 +27,7 @@ class Solution {
         }
         return minSum;
     }
-    
+    //O(row) Tc O(row*(maxCol)) Sc
     public int memo(List<List<Integer>> a,int row,int col,int[][] dp){
         if(row==a.size())return dp[row][col]=0;
         if(col>=a.get(row).size())return Integer.MAX_VALUE;
@@ -39,6 +41,21 @@ class Solution {
             minSum=min(sum1,sum2,minSum);
         }
         return dp[row][col]=minSum;
+    }
+    //O(row) tc O(row)Sc
+    public int tabulation(List<List<Integer>> a){
+        int[] dp=new int[a.size()+1];//dp on col
+        int n=a.size();
+        int m=a.get(n-1).size();
+        for(int col=0;col<m;col++){
+            dp[col]=a.get(n-1).get(col);
+        }
+        for(int row=n-2;row>=0;row--){//n-2 because we already stored last row values in dp
+            for(int col=0;col<row+1;col++){
+                dp[col]=Math.min(dp[col],dp[col+1])+a.get(row).get(col);
+            }
+        }
+        return dp[0];
     }
     
 }
