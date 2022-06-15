d2141225cefba1867cf613f05c79aa74cd939347
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-15 22:57:17 +0530

    Time: 24 ms (59.43%), Space: 63.5 MB (51.07%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -0,0 +1,49 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        int dist;
+        
+        Pair(int row,int col,int dist){
+            this.row=row;
+            this.col=col;
+            this.dist=dist;
+        }
+    }
+    public int maxDistance(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        Queue<Pair> q=new ArrayDeque();
+        
+        boolean[][] vis=new boolean[n][m];
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+                if(mat[r][c]==1){
+                    vis[r][c]=true;
+                    q.add(new Pair(r,c,0));
+                }
+            }
+        }
+        if(q.size()==0 || q.size()==m*n)return -1;
+        int maxDist=0;
+        int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
+        while(q.size()!=0){
+            int size=q.size();
+            while(size-- >0){
+                Pair rPair=q.remove();
+                for(int i=0;i<4;i++){
+                    int nr=rPair.row+dir[i][0];
+                    int nc=rPair.col+dir[i][1];
+                    int dist=rPair.dist+1;
+                    
+                    if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==false){
+                        maxDist=dist;
+                        q.add(new Pair(nr,nc,dist));
+                        vis[nr][nc]=true;
+                    }
+                }
+            }
+        }
+        return maxDist;
+    }
+}
