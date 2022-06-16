954265b3ed58055cef83ee1c4db6ac18a6edc56c
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-16 21:12:50 +0530

    Time: 40 ms (10.53%), Space: 84.7 MB (5.52%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -14,12 +14,12 @@ class Solution {
         int n=mat.length;
         int m=mat[0].length;
         Queue<Pair> q=new ArrayDeque();
-        
+        int[][] ans=new int[n][m];
         boolean[][] vis=new boolean[n][m];
         for(int r=0;r<n;r++){
             for(int c=0;c<m;c++){
                 if(mat[r][c]==0){
-                    vis[r][c]=true;
+                    
                     q.add(new Pair(r,c,0));
                 }
             }
@@ -27,22 +27,24 @@ class Solution {
         
         int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
         while(q.size()!=0){
-            int size=q.size();
-            while(size-- >0){
-                Pair rPair=q.remove();
-                for(int i=0;i<4;i++){
-                    int nr=rPair.row+dir[i][0];
-                    int nc=rPair.col+dir[i][1];
-                    int dist=rPair.dist+1;
-
-                    if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==false){
-                        mat[nr][nc]=dist;
+            Pair rPair=q.remove();
+            
+            if(vis[rPair.row][rPair.col]==true){
+                continue;
+            }
+            vis[rPair.row][rPair.col]=true;
+            ans[rPair.row][rPair.col]=rPair.dist;
+            
+            for(int i=0;i<4;i++){
+                int nr=rPair.row+dir[i][0];
+                int nc=rPair.col+dir[i][1];
+                int dist=rPair.dist+1;
+                
+                if(nr>=0 && nc>=0 && nr<mat.length && nc<mat[0].length && vis[nr][nc]==false){
                         q.add(new Pair(nr,nc,dist));
-                        vis[nr][nc]=true;
-                    }
                 }
             }
         }
-        return mat;
+        return ans;
     }
 }
