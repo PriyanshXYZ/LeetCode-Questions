10698a413e282a63423c057b1da39e8c8e927025
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-17 00:55:32 +0530

    Time: 3 ms (16.97%), Space: 53.9 MB (73.08%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -10,6 +10,72 @@ class Solution {
     
     int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
     
+    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        int n=grid.length;
+        int m=grid[0].length;
+        
+        boolean[][] visited=new boolean[n][m];
+        List<Pair> border=getBorderElemOfConnectedComp(grid,row,col,grid[row][col],visited);
+        
+        for(Pair p:border){
+            grid[p.r][p.c]=color;
+        }
+        return grid;
+    }
+    
+    
+    public List<Pair> getBorderElemOfConnectedComp(int[][] grid,int r,int c,int prevColor,boolean[][] visited){
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]!=prevColor || visited[r][c]==true)return new ArrayList();
+        
+        List<Pair> ans=new ArrayList();
+        visited[r][c]=true;
+        for(int i=0;i<4;i++){
+            int nr=r+dir[i][0];
+            int nc=c+dir[i][1];
+            
+            List<Pair> next=getBorderElemOfConnectedComp(grid,nr,nc,prevColor,visited);
+            if(next.size()>0){
+                ans.addAll(next);
+            }
+            
+        }
+        if(isBorder(grid,r,c,prevColor)){
+            ans.add(new Pair(r,c));
+        }
+        return ans;
+    }
+     public boolean isBorder(int[][] grid,int i,int j,int color){
+        if(i==0 || j==0 || j==grid[0].length-1 || i==grid.length-1){
+            return true;
+        }
+        if(grid[i-1][j]!=color){
+            return true;
+        }else if(grid[i+1][j]!=color){
+            return true;
+        }else if(grid[i][j-1]!=color){
+            return true;
+        }else if(grid[i][j+1]!=color){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
+
+//using BFS
+class Solution1 {
+    class Pair{
+        int r;
+        int c;
+        Pair(int r,int c){
+            this.r=r;
+            this.c=c;
+        }
+    }
+    
+    int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
+    
     
     public int[][] colorBorder(int[][] grid, int row, int col, int color) {
         int prevColor=grid[row][col];
