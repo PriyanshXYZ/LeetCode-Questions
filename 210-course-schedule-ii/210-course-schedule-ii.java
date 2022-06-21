d0492f6fe02f4bc74d5a539de63d366e844460e8
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-22 00:44:32 +0530

    Time: 11 ms (44.87%), Space: 49 MB (59.49%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] a) {
+        //reverse topological 
+        List<Integer>[] graph=new ArrayList[numCourses];
+        
+        for(int i=0;i<numCourses;i++){
+            graph[i]=(new ArrayList());
+        }
+        //creating a graph...
+        for(int[] req:a){
+            graph[req[0]].add(req[1]);
+        }
+        
+        Queue<Integer> q=new ArrayDeque();
+        
+        //getting the in degree of the vtces of the graph
+        int[] inDeg=new int[numCourses];
+        for(int i=0;i<graph.length;i++){
+            for(int nbr:graph[i]){
+                inDeg[nbr]++;
+            }
+        }
+        
+        //adding vtces which are not dependancy
+        for(int i=0;i<inDeg.length;i++){
+            if(inDeg[i]==0){
+                q.add(i);
+            }
+        }
+        
+        int[] arr=new int[numCourses];
+        int idx=numCourses-1;
+        while(!q.isEmpty()){
+            int rem=q.remove();
+            arr[idx--]=rem;
+            
+            for(int nbr:graph[rem]){
+                if(inDeg[nbr]>0){
+                    inDeg[nbr]--;
+                }
+                if(inDeg[nbr]==0){
+                    q.add(nbr);
+                }
+            }
+        }
+        
+        if(idx==-1)return arr;
+        else return new int[]{};
+    }
+}
