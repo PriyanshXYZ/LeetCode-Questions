ef68c80d54cb0d701218e171cba4954b311ee2fc
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-25 00:43:13 +0530

    Time: 18 ms (23.23%), Space: 58.2 MB (27.27%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPossible(int[] target) {
+        if(target.length==1)return target[0]==1;
+        
+        //max heap
+        PriorityQueue<Integer> pq=new PriorityQueue(Collections.reverseOrder());
+        int sum=0;
+        for(int item:target){
+            pq.add(item);
+            sum+=item;
+        }
+        
+        while(pq.peek()!=1){
+            int curr=pq.remove();
+            
+            if(sum-curr==1)return true;
+            
+            
+            int x=curr%(sum-curr);
+            sum=sum-curr+x;
+            
+            if(x==0 || x==curr)return false;
+            else{
+                pq.add(x);
+            }   
+        }
+        return true;
+    }
+}
