8a9f029cb6454972cd359072e5f2bb0fee455fbb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-26 11:03:36 +0530

    Time: 2 ms (94.06%), Space: 50.5 MB (96.24%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,4 +1,32 @@
-class Solution {
+class Solution{
+    //o(n-k) o(n)
+    public int maxScore(int[] a, int k) {
+        if(a.length==1)return a[0];
+        int n=a.length;
+        if(k==1){
+            return Math.max(a[0],a[n-1]);
+        }
+        int[] prefix=new int[n];
+        prefix[0]=a[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+a[i];
+        }
+        if(k==n)return prefix[n-1];
+        int size=n-k;
+        int smallestSum=prefix[size-1];
+        
+        int i=1;
+        while(i+size-1<n){
+            int currSum=prefix[i+size-1]-prefix[i-1];
+            smallestSum=Math.min(smallestSum,currSum);
+            
+            i++;
+        }
+        return prefix[n-1]-smallestSum;
+    }
+}
+//o(n) o(n);
+class Solution1 {
     public int maxScore(int[] a, int k) {
         if(a.length==1)return a[0];
         int n=a.length;
@@ -23,31 +51,5 @@ class Solution {
             maxSum=Math.max(maxSum,prefix[i]+suffix[n-k+i]);
         }
         return maxSum;
-    }
-}
-class Solution1{
-    public int maxScore(int[] a, int k) {
-        if(a.length==1)return a[0];
-        int n=a.length;
-        if(k==1){
-            return Math.max(a[0],a[n-1]);
-        }
-        int[] prefix=new int[n];
-        prefix[0]=a[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+a[i];
-        }
-        if(k==n)return prefix[n-1];
-        int size=n-k;
-        int smallestSum=prefix[size-1];
-        
-        int i=1;
-        while(i+size-1<n){
-            int currSum=prefix[i+size-1]-prefix[i-1];
-            smallestSum=Math.min(smallestSum,currSum);
-            
-            i++;
-        }
-        return prefix[n-1]-smallestSum;
     }
 }
