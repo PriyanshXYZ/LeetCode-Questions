2cf24df703015060ad42fe5c1269a97980530f02
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-26 11:02:02 +0530

    Time: 6 ms (9.64%), Space: 67.7 MB (5.03%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -5,6 +5,33 @@ class Solution {
         if(k==1){
             return Math.max(a[0],a[n-1]);
         }
+        int[] prefix=new int[n+1];
+        int[] suffix=new int[n+1];
+        
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i]+a[i];
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            suffix[i]=suffix[i+1]+a[i];
+        }
+        
+        if(k==n)return prefix[n];
+        int maxSum=0;
+        for(int i=0;i<=k;i++){
+            
+            maxSum=Math.max(maxSum,prefix[i]+suffix[n-k+i]);
+        }
+        return maxSum;
+    }
+}
+class Solution1{
+    public int maxScore(int[] a, int k) {
+        if(a.length==1)return a[0];
+        int n=a.length;
+        if(k==1){
+            return Math.max(a[0],a[n-1]);
+        }
         int[] prefix=new int[n];
         prefix[0]=a[0];
         for(int i=1;i<n;i++){
