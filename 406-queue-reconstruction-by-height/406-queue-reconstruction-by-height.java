fd28be5446bd946cd38d1d8a34f8eaf71b68f21d
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-30 02:30:43 +0530

    Time: 16 ms (24.13%), Space: 43.1 MB (88.94%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        
+        Arrays.sort(people,new Comparator<int[]>(){
+            
+            @Override
+            public int compare(int[] a,int[] b){
+                if(a[0]==b[0]){
+                    return a[1]-b[1];
+                }else{
+                    return b[0]-a[0];
+                }
+            }
+        });
+        for(int i=0;i<people.length;i++){
+            int count=0;
+            for(int j=0;j<i;j++){
+                if(people[j][0]>=people[i][0])count++;
+                if(count>people[i][1]){
+                    swap(people,i,j);
+                    break;
+                }
+            }
+        }
+        return people;
+    }
+    public void swap(int[][] a,int i,int j){
+        int[] temp=a[i];
+        
+        for(int k=i;k>j;k--){
+            a[k]=a[k-1];
+        }
+        a[j]=temp;
+    }
+}
