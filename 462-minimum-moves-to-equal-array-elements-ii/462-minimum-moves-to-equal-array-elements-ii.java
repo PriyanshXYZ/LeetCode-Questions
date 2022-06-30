715d9c4f641207c6bdd90ec3c91c74eb50b83905
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-06-30 12:04:38 +0530

    Time: 997 ms (5.04%), Space: 45.5 MB (68.68%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        if(nums.length==1)return 0;
+        int prev=0;
+        long minDiffSum=Integer.MAX_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+            prev=nums[i];
+            long currDiff=0;
+            for(int j=0;j<nums.length;j++){
+                // if(nums[j]>0){
+                    if(nums[j]-prev>=0){
+                        currDiff+=nums[j]-prev;
+                    }else{
+                        currDiff+=prev-nums[j];
+                    }
+                // }else{
+                    
+                // }
+            }
+            
+            // System.out.println(currDiff);
+            minDiffSum=Math.min(currDiff,minDiffSum);
+        }
+        return (int)minDiffSum;
+    }
+}
