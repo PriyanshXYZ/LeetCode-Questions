3744ec68c56b0eb88611cfd803643e16a34d5642
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-01 15:59:22 +0530

    Added solution - LeetHub

---
+++ 10. Maximum Path Sum between 2 Leaf Nodes - GFG/10.-maximum-path-sum-between-2-leaf-nodes.java
@@ -0,0 +1,155 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.maxPathSum(root));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Solution
+{
+    int maxSum;
+    int maxPathSum(Node root)
+    { 
+        // code here
+        maxSum=Integer.MIN_VALUE;
+        
+        int sum=sum(root);
+        if(root.left!=null && root.right!=null){
+            return maxSum;    
+        }else{
+            return Math.max(maxSum,sum);
+        }
+        
+    } 
+    
+    int sum(Node root){
+        if(root==null)return Integer.MIN_VALUE;
+        
+        //leaf
+        if(root.left==null && root.right==null){
+            return root.data;
+        }
+        
+        int leftSum=sum(root.left);
+        int rightSum=sum(root.right);
+        
+        int currSum=0;
+        if(root.left!=null && root.right!=null){
+            currSum=leftSum+rightSum+root.data;
+            maxSum=Math.max(maxSum,currSum);
+        }
+        
+        
+        return Math.max(leftSum,rightSum)+root.data;
+    }
+}
