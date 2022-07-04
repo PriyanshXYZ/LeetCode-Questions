e40030dacd1e6c3e1f776c3119065df232a87c4d
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-04 21:53:10 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,94 @@
+// { Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        parent=new int[101];//for 0 idxed job id
+        for(int i=0;i<101;i++){
+            parent[i]=i;
+        }
+        
+        Arrays.sort(arr,new Comparator<Job>(){
+            @Override
+            public int compare(Job a,Job b){
+                    return b.profit-a.profit;
+            }
+        });
+        int ans=0;
+        int jobs=0;
+        for(Job j:arr){
+            int lead=find(j.deadline);
+            if(lead>0){
+                ans+=j.profit;
+                jobs++;
+                parent[lead]=find(lead-1);
+            }
+        }
+        return new int[]{jobs,ans};
+    }
+    int[] parent;
+    
+    public int find(int x){
+        if(parent[x]==x){
+            return x;
+        }else{
+            parent[x]=find(parent[x]);
+            return parent[x];
+        }
+    }
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
