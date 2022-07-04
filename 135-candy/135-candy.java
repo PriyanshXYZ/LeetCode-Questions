5ba57d7ed576102fa529be2c8004ed209b55df28
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-04 16:35:26 +0530

    Time: 5 ms (45.91%), Space: 51.2 MB (76.43%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,4 +1,29 @@
 class Solution {
+    //using single array
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] candies=new int[n];
+        Arrays.fill(candies,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i]=Math.max(candies[i],candies[i+1]+1);
+            }
+        }
+        int sum=0;
+        for(int candy:candies){
+            sum+=candy;
+        }
+        return sum;
+    }
+}
+class Solution1 {
+    //O(n) tc and sc
     public int candy(int[] ratings) {
         
         int n=ratings.length;
