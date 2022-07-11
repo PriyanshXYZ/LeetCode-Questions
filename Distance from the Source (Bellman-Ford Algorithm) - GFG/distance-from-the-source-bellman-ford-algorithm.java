0ca9919ce6834124b68716264e6a5bf28248039c
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-11 23:58:47 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.java
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+    
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            
+            int i=0;
+            while (i++<E) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                ArrayList<Integer> t1 = new ArrayList<>();
+                t1.add(u);
+                t1.add(v);
+                t1.add(w);
+                adj.add(t1);
+            }
+            
+            int S = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            
+            int[] ptr = ob.bellman_ford(V, adj, S);
+            
+            for(i=0; i<V; i++)
+                System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+*   adj: vector of vectors which represents the graph
+*   S: source vertex to start traversing graph with
+*   V: number of vertices
+*/
+class Solution
+{
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> adj, int S)
+    {
+        // Write your code here
+        int[] res=new int[V];
+        Arrays.fill(res,(int)1e8);
+        res[S]=0;
+        for(int i=0;i<V-1;i++){
+            for(ArrayList<Integer> edge:adj){
+                int u=edge.get(0);
+                int v=edge.get(1);
+                int wt=edge.get(2);
+                
+                if(res[u]==(int)1e8){
+                    continue;
+                // }else if(res[v]==(int)1e8){
+                //     res[v]=wt;
+                }else{
+                    res[v]=Math.min(res[u]+wt,res[v]);
+                }
+            }
+        }
+        return res;
+    }
+}
+
