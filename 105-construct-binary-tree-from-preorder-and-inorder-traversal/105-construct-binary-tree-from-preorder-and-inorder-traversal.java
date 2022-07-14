f884f301450bbe1f2f188c755d6c765356754575
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-14 10:15:59 +0530

    Time: 4 ms (64.48%), Space: 44.7 MB (28.97%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,23 +15,29 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode root=construct(0,preorder.length-1,0,inorder.length-1,preorder,inorder);
-        return root;
-    }
-    public TreeNode construct(int pre_lo,int pre_hi,int in_lo,int in_hi,int[] pre,int[] in){
-        if(pre_lo>pre_hi || in_lo>in_hi)return null;
-        TreeNode root=new TreeNode(pre[pre_lo]);
-        //to get position of  curr node in the inorder array
-        //can also be done using hashmap
-        int idx=in_lo;
-        while(pre[pre_lo]!=in[idx]){
-            idx++;
+        HashMap<Integer,Integer> map=new HashMap();
+        int n=preorder.length;
+        for(int i=0;i<n;i++){
+            map.put(inorder[i],i);
         }
-        //leftSubtreeSizeIndex
-        int lhs=idx-in_lo;
-        root.left=construct(pre_lo+1,pre_lo+lhs,in_lo,idx-1,pre,in);
-        root.right=construct(pre_lo+lhs+1,pre_hi,idx+1,in_hi,pre,in);
+        TreeNode root=construct(0,n-1,0,n-1,preorder,map);
+        
         return root;
     }
     
+    public TreeNode construct(int pre_lo,int pre_hi,int in_lo,int in_hi,int[] pre,HashMap<Integer,Integer> in){
+        //base case
+        if(pre_lo>pre_hi || in_lo>in_hi)return null;
+        TreeNode root=new TreeNode(pre[pre_lo]);
+        
+        int idx=in_lo;
+        if(pre_lo+1<=pre_hi){
+            idx=in.get(pre[pre_lo]);
+        }
+        int lhsize=idx-in_lo;
+        
+        root.left=construct(pre_lo+1,pre_lo+lhsize,in_lo,idx-1,pre,in);
+        root.right=construct(pre_lo+lhsize+1,pre_hi,idx+1,in_hi,pre,in);
+        return root;
+    }
 }
