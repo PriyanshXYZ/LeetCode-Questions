de2159519c880f55c477032722aaaa8554b42a59
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-15 23:58:22 +0530

    Time: 1 ms (100.00%), Space: 57 MB (12.33%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,4 +1,24 @@
 class Solution {
+    public int majorityElement(int[] a) {
+        int majEle=a[0];
+        int freq=1;
+        for(int i=1;i<a.length;i++){
+            if(a[i]==majEle){
+                freq++;
+            }else{
+                if(freq>0){
+                    freq--;
+                }else{
+                    majEle=a[i];
+                    freq=1;
+                }
+            }
+        }
+        return majEle;
+    }
+}
+//with space
+class Solution1 {
     public int majorityElement(int[] nums) {
         HashMap<Integer,Integer> map=new HashMap();
         for(int i=0;i<nums.length;i++){
