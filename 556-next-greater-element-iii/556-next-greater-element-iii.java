077d1521e70521ad6d5854b41bc99ff54c41d8c5
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-15 23:12:37 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (62.41%) - LeetHub

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        String s=Integer.toString(n);
+        char[] a=s.toCharArray();
+        int i=a.length-1;
+        while(i>0){
+            if(a[i-1]>=a[i]){
+                i--;
+            }else{
+                break;
+            }
+        }
+        if(i==0)return -1;
+        
+        int j=a.length-1;
+        while(j>=i){
+            if(a[j]>a[i-1]){
+                break;
+            }else{
+                j--;
+            }
+        }
+        
+        swap(a,i-1,j);//i-1 because  we were checking for idx-1 val
+        
+        reverse(a,i,a.length-1);
+        
+        String ans=String.valueOf(a);
+        long res=Long.parseLong(ans);
+        if(res>Integer.MAX_VALUE){
+            return -1;
+        }
+        return (int)res;
+    }
+    void reverse(char[] a,int i,int j){
+        while(i<j){
+            swap(a,i,j);
+            i++;
+            j--;
+        }
+    }
+    void swap(char[] a,int i,int j){
+        char temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+    }
+}
