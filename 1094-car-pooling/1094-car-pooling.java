811ae0f0c44d6b4a5ae52df327626636783cfbc9
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-22 01:06:10 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (77.58%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,5 +1,27 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
+        
+        int[] stops=new int[1001];
+        for(int[] trip:trips){
+            int ppl=trip[0];
+            int from=trip[1];
+            int to=trip[2];
+            
+            stops[from]+=ppl;
+            stops[to]-=ppl;
+        }
+        int count=0;
+        for(int i=0;i<1001;i++){
+            count+=stops[i];
+            
+            if(count>capacity)return false;
+        }
+        return true;
+    }
+}
+class Solution1 {
+    //o(nlogn)
+    public boolean carPooling(int[][] trips, int capacity) {
         int[] map=new int[1001];
         TreeSet<Integer> stops=new TreeSet();
         int currPassenger=0;
