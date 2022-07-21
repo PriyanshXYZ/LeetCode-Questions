7adbd9df425346aa77d5e8d8e0d141c3416ae893
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-21 21:14:21 +0530

    Time: 20 ms (13.89%), Space: 50.6 MB (14.02%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -7,12 +7,15 @@ class Solution {
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                map.putIfAbsent(i-j,new PriorityQueue());
-                map.get(i-j).add(mat[i][j]);
+                map.put(i-j,new PriorityQueue());
             }
         }
         
-        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                map.get(i-j).add(mat[i][j]);    
+            }
+        }
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
