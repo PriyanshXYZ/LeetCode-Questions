839b01b661926975138c0c6f337e95dfba1627c4
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-27 20:07:24 +0530

    Time: 1 ms (78.58%), Space: 42.7 MB (52.44%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,6 +14,27 @@
  * }
  */
 class Solution {
+    //using O(1) space
+    
+    public void flatten(TreeNode root) {
+        // TreeNode prev=null;
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode prev=curr.left;
+                while(prev.right!=null){
+                    prev=prev.right;
+                }
+                prev.right=curr.right;
+                curr.right=curr.left;
+                curr.left=null;
+            }
+            curr=curr.right;
+        }
+    }
+}
+class Solution1 {
+    //O(n) time and space
     TreeNode prev;
     public void flatten(TreeNode root) {
         if(root==null)return;
