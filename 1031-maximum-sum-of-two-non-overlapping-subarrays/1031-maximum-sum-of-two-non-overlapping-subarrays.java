2083f51faa6e1d4b7199d511b05ca7fdbced6cf7
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-07-29 17:56:27 +0530

    Time: 191 ms (5.02%), Space: 49.1 MB (5.02%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.java
@@ -33,7 +33,15 @@ class Solution {
             }
         }
         
+        for(int i=0;i<n;i++){
+            System.out.print(leftMax[i]+" ");
+        }
+        System.out.println();
         
+        for(int i=0;i<n;i++){
+            System.out.print(rightMax[i]+" ");
+        }
+        System.out.println();
         
         int maxSum=0;
         for(int i=fl-1;i<n-sl;i++){
