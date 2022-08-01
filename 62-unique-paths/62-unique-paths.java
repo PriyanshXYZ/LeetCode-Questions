4f9efa7a099864f17d48af9330b2a89b6dee13ed
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-08-02 01:47:31 +0530

    Time: 1 ms (55.29%), Space: 40.9 MB (57.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -5,25 +5,26 @@ class Solution {
         row=m;
         col=n;
         int[][] dp=new int[m][n];
-        boolean[][] vis=new boolean[m][n];
+        
         for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        return paths(0,0,vis,dp);
+        return paths(0,0,dp);
     }
-    public int paths(int r,int c,boolean[][] vis,int[][] dp){
+    public int paths(int r,int c,int[][] dp){
         if(r==row-1 && c==col-1)return 1;
         
         if(dp[r][c]!=-1)return dp[r][c];
         
-        vis[r][c]=true;
+        
         int ans=0;
         if(r+1<row){
-            ans+=paths(r+1,c,vis,dp);
+            ans+=paths(r+1,c,dp);
         }
         if(c+1<col){
-            ans+=paths(r,c+1,vis,dp);
+            ans+=paths(r,c+1,dp);
         }
+        
         
         return dp[r][c]=ans;
     }
