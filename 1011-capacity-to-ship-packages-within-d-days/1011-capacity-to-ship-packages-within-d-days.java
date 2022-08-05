df6b8e1c121c03892470f06df24f6234669dbbc3
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-08-05 22:58:38 +0530

    Time: 12 ms (72.42%), Space: 53.9 MB (70.43%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int hac=0;
+        int lac=Integer.MIN_VALUE;
+        
+        for(int i=0;i<weights.length;i++){
+            hac+=weights[i];
+            lac=Math.max(lac,weights[i]);
+        }
+        
+        while(lac<hac){
+            int limit=(hac+lac)/2;
+            
+            int partitions=getPartitions(weights,limit);
+            
+            if(partitions>days){
+                lac=limit+1;
+            }else {
+                hac=limit;
+            }
+        }
+        return lac;
+    }
+    private int getPartitions(int[] weights,int limit){
+        int partition = 1;
+        int currPartWt = 0;
+        for(int wt:weights){
+            if(currPartWt + wt <= limit ){
+                currPartWt+=wt;
+            }else{
+                currPartWt=wt;
+                partition++;
+            }
+        }
+        return partition;
+    }
+}
