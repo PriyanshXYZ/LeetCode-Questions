3ce38c6babca7668ba707e5279089c9ac1e79836
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-08-05 16:28:57 +0530

    Time: 3 ms (92.57%), Space: 71.9 MB (25.50%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int cz=0;
+        
+        int len=0;
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                cz++;
+            }
+            
+            while(cz>k){
+                if(nums[j]==0){
+                    cz--;
+                }
+                j++;
+            }
+            // System.out.println(i+" "+j+" "+cz);
+            len=Math.max(len,i-j+1);
+        }
+        
+        return len;
+    }
+}
