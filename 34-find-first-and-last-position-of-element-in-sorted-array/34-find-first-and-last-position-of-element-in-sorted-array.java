ae351f222c149049525200438472479e35d50057
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-08-18 19:42:36 +0530

    Time: 0 ms (100.00%), Space: 46.6 MB (82.82%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,34 @@
 class Solution {
-    //if we are not updating like mid+1 or mid-1 for strt or end indx then make sure that iterating condition must be such that i and j must not be adjecant;
-    public int[] searchRange(int[] a, int target) {
-        if(a.length==0)return new int[]{-1,-1};
-        return new int[]{ firstIdxbs(a,target), lastIdxbs(a,target)};
-    }
-    public int firstIdxbs(int[] a,int target){
-        int i=0,j=a.length-1;
-        int idx=-1;
-        while(i<j-1){
-            int mid=i+(j-i)/2;
-            if(a[mid]>=target){
-                //update j drop i
-                j=mid;
+    public int[] searchRange(int[] nums, int target) {
+        if(nums.length==0)return new int[]{-1,-1};
+        int lo=0;
+        int hi=nums.length-1;
+        
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            
+            if(nums[mid]<target){
+                lo=mid+1;
             }else{
-                i=mid;
+                hi=mid;
             }
         }
-        if(a[i]==target) return i;
-        if(a[j]==target) return j;
-        return idx;
-    }
-     public int lastIdxbs(int[] a,int target){
-        int i=0,j=a.length-1;
-        int idx=-1;
-        while(i<j-1){
-            int mid=i+(j-i)/2;
-            if(a[mid]<=target){
-                //update i drop j
-                i=mid;
+        if(nums[lo]!=target){
+            return new int[]{-1,-1};
+        }
+        int left=lo;
+        int right=0;
+        hi=nums.length-1;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2+1;
+            
+            if(nums[mid]>target){
+                hi=mid-1;
             }else{
-               j=mid;
+                lo=mid;
             }
         }
-        if(a[j]==target) return j;
-        if(a[i]==target) return i;
-        return idx;
+        right=lo;
+        return new int[]{left,right};
     }
 }
