4794be9f3a9dc84e0603e583cd1aee059f6bac0e
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-08-24 00:42:23 +0530

    Time: 4 ms (72.50%), Space: 42.6 MB (96.66%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -2,30 +2,43 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         //start from reverse
         int n=matrix.length;
+        int m=matrix[0].length;
         
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<n;j++){
+        int[] dig=new int[n];
+        //fill
+        for(int i=0;i<m;i++){
+            dig[i]=matrix[0][i];
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     System.out.print(dig[i]+" ");
+        // }
+        // System.out.println();
+        
+        for(int i=1;i<n;i++){
+            int[] temp=new int[m];
+            
+            for(int j=0;j<m;j++){
                 int min=Integer.MAX_VALUE;
-                if((j-1)>=0){
-                    min=Math.min(min,matrix[i+1][j-1]);
+                if(j-1>=0){
+                    min=Math.min(min,dig[j-1]);
                 }
-                
-                min=Math.min(min,matrix[i+1][j]);
-                
-                if((j+1)<n){
-                    min=Math.min(min,matrix[i+1][j+1]);
+                min=Math.min(min,dig[j]);
+                if(j+1<m){
+                    min=Math.min(min,dig[j+1]);
                 }
-                matrix[i][j]+=min;
+                temp[j]=min+matrix[i][j];
             }
-            // for(int j=0;j<n;j++){
-            //     System.out.print(matrix[i][j]+" ");
+            dig=temp;
+            // for(int k=0;k<n;k++){
+            //     System.out.print(dig[k]+" ");
             // }
             // System.out.println();
         }
         
         int ans=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            ans=Math.min(ans,matrix[0][i]);
+        for(int i=0;i<m;i++){
+            ans=Math.min(ans,dig[i]);
         }
         return ans;
     }
