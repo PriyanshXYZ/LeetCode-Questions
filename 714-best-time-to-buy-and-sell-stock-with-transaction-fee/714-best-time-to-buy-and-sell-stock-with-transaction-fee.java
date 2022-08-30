04b474548c8569d76d0eba11d85b1d00fdfbb1d1
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-08-30 23:11:01 +0530

    Time: 52 ms (19.75%), Space: 75.6 MB (42.14%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,31 +1,30 @@
 class Solution {
-    public int maxProfit(int[] stock, int fees) {
-        int n=stock.length;
-        int[][] dp=new int[n+2][2];
-        for(int idx=n;idx>=0;idx--){
+    final int SELL=1;
+    final int BUY=0;
+    public int maxProfit(int[] prices, int fee) {
+        int n=prices.length;
+        int[][] dp =new int[n+1][2];//2 states buy / sell
+        
+        for(int i=n;i>=0;i--){
             for(int flag=0;flag<2;flag++){
-                
-                    if(idx==n){
-                        dp[idx][flag]=0;
-                        continue;
-                    }
-
-                    if(flag==1){
-                    //buy
-                        int profitWhenBuyingHere=-stock[idx] + dp[idx+1][0];
-                        int profitWhenNotBuyinhHere=dp[idx+1][1];
-                        dp[idx][flag]=Math.max(profitWhenBuyingHere,profitWhenNotBuyinhHere);
-                    }else{
-                    //sell
-                        int profitWhenSellingHere=+stock[idx] + dp[idx+1][1]-fees;//transaction complete
-                        int profitWhenNotSellingHere=dp[idx+1][0];
-                        
-                        dp[idx][flag]=Math.max(profitWhenSellingHere,profitWhenNotSellingHere);
-                    }
+                if(i==n){
+                    dp[i][flag]=0;
+                    continue;
                 }
-            
+                
+                if(flag==SELL){
+                    int profitWhenSellingHere=-prices[i]+dp[i+1][BUY];
+                    int profitWhenNotSellingHere=dp[i+1][SELL];
+                    
+                    dp[i][SELL]=Math.max(profitWhenSellingHere,profitWhenNotSellingHere);
+                }else{
+                    int profitWhenBuyingHere=prices[i]+dp[i+1][SELL]-fee;
+                    int profitWhenNotBuyingHere=dp[i+1][BUY];
+                    
+                    dp[i][BUY]=Math.max(profitWhenBuyingHere,profitWhenNotBuyingHere);
+                }
+            }
         }
-        return dp[0][1];
-
+        return dp[0][SELL];
     }
 }
