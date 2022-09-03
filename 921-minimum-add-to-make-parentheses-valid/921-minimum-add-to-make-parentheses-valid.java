c78739c7eac8cbd63ee3e72651035e8709cc79ec
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-03 21:21:25 +0530

    Time: 3 ms (34.08%), Space: 41.6 MB (78.65%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -18,3 +18,16 @@ class Solution {
         return stk.size();
     }
 }
+class Solution1 {
+    public int minAddToMakeValid(String s) {
+        int open=0,close=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') open++;
+            else{
+                if(open==0) close++;
+                else open--;
+            }
+        }
+        return open+close;
+    }
+}
