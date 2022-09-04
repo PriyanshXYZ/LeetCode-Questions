52f9289e7931bf319831b8f061e3cf59f2cb8073
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-04 16:48:56 +0530

    Time: 18 ms (8.12%), Space: 49.9 MB (7.76%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,5 +1,26 @@
 class Solution {
     public int trap(int[] height) {
+        int ans=0;
+        int idx=0;
+        
+        Stack<Integer> stk=new Stack();
+        while(idx<height.length){
+            while(stk.size()>0 && height[idx]>height[stk.peek()]){
+                int top=stk.pop();
+                if(stk.isEmpty()){
+                    break;
+                }
+                int dist=idx-stk.peek()-1;
+                int ht=Math.min(height[idx],height[stk.peek()])-height[top];
+                ans+=dist*ht;
+            }
+            stk.push(idx++);
+        }
+        return ans;
+    }
+}
+class Solution1 {
+    public int trap(int[] height) {
         int n=height.length;
         int[] leftMax=new int[n];
         for(int i=0;i<n;i++){
