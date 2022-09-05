f0a283994ceb27224ae530be83c3281192253357
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-05 20:49:23 +0530

    Time: 4 ms (70.69%), Space: 49.7 MB (57.84%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,4 +1,40 @@
 class Solution {
+    //O(log(n+m))
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        
+        int e1=(n+m+1)/2;
+        int e2=(n+m+2)/2;
+        
+        int median1=findKth(nums1,0,nums2,0,e1);
+        int median2=findKth(nums1,0,nums2,0,e2);
+        
+        return (median1+median2)/2.0;
+    }
+    private int findKth(int[] A, int aStart, int[] B, int bStart, int k){
+        if(aStart >= A.length){
+            return B[bStart + k - 1];
+        }
+        if(bStart >= B.length){
+            return A[aStart + k - 1];
+        }
+        if(k == 1){
+            return Math.min(A[aStart], B[bStart]);
+        }
+        int aMid = aStart + k/2 - 1;
+        int bMid = bStart + k/2 - 1;
+        int aVal = aMid >= A.length ? Integer.MAX_VALUE : A[aMid];
+        int bVal = bMid >= B.length ? Integer.MAX_VALUE : B[bMid];
+        if(aVal <= bVal){
+            return findKth(A, aMid + 1, B, bStart, k - k/2);
+        }else{
+            return findKth(A, aStart, B, bMid + 1, k - k/2);
+        }
+    }
+}
+class Solution1 {
+    //O(n+m)
     public int[] merge2Arrays(int[] n1,int[] n2){
         int n=n1.length+n2.length;
         int[] a=new int[n];
