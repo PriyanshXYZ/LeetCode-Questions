7d04587c37da7dc84f787cbcc552adf637d36d14
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-10 14:39:41 +0530

    Time: 160 ms (17.45%), Space: 84.9 MB (11.41%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        long sum=0;
+        long lo=1;
+        long hi=0;
+        for(int i=0;i<candies.length;i++){
+            sum+=candies[i];
+            hi=Math.max(candies[i],hi);
+        }
+        if(k>sum)return 0;
+        
+        while(lo<hi){
+            long mid=lo+(hi-lo+1)/2;
+            
+            long size=0;
+            for(int val:candies){
+                
+                long div = (val) / mid;
+
+                size+=div;
+                
+            }
+            
+            if(size<k){
+                hi=mid-1;
+            }else{
+                lo=mid;
+            }
+        }
+        return (int)(lo);
+    }
+}
