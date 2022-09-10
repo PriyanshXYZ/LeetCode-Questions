d85155d607b2429a7b92db6c6e28941cb496adf9
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-10 11:36:33 +0530

    Time: 26 ms (12.46%), Space: 46.4 MB (6.11%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,37 +1,50 @@
 class Solution {
-    public int maxProfit(int k, int[] stock) {
-        int n=stock.length;
-        int[][] ahead=new int[2][k+1];
-        for(int idx=n;idx>=0;idx--){
-            int[][] curr=new int[2][k+1];
-            for(int flag=0;flag<2;flag++){
-                for(int trCount=0;trCount<=k;trCount++){
-                    if(trCount==0){
-                        curr[flag][trCount]=0;     
-                        continue;
-                    }
-
-                    if(idx==n){
-                        curr[flag][trCount]=0;
-                        continue;
-                    }
-
-                    if(flag==1){
-                    //buy
-                        int profitWhenBuyingHere=-stock[idx] + ahead[0][trCount];
-                        int profitWhenNotBuyinhHere=ahead[1][trCount];
-                        curr[flag][trCount]=Math.max(profitWhenBuyingHere,profitWhenNotBuyinhHere);
-                    }else{
-                    //sell
-                        int profitWhenSellingHere=+stock[idx] + ahead[1][trCount-1];//transaction complete
-                        int profitWhenNotSellingHere=ahead[0][trCount];
-                        
-                        curr[flag][trCount]=Math.max(profitWhenSellingHere,profitWhenNotSellingHere);
-                    }
-                }
+    int BUY=0;
+    int SELL=1;
+    public int maxProfit(int k, int[] prices) {
+        int[][][] dp=new int[prices.length+1][k+1][2];
+        for(int i=0;i<=prices.length;i++){
+            for(int j=0;j<=k;j++){
+                Arrays.fill(dp[i][j],-1);
             }
-            ahead=curr;
         }
-        return ahead[1][k];
+        return memo(0,prices,dp,k,BUY);
+        // return recursion(0,prices,k,BUY);
     }
+    //time complexity O(2^n) space is recursion call stack
+    public int recursion(int idx,int[] prices,int k,int curr){
+        if(k==0)return 0;
+        if(idx==prices.length)return 0;
+        
+        if(curr==BUY){
+            int buyingCurrStock=recursion(idx+1,prices,k,SELL)-prices[idx];
+            int notBuyingCurrStock=recursion(idx+1,prices,k,BUY);
+            return Math.max(buyingCurrStock,notBuyingCurrStock);
+        }else{
+            int sellingCurrStock=recursion(idx+1,prices,k-1,BUY)+prices[idx];
+            int notSellingCurrStock=recursion(idx+1,prices,k,SELL);
+            return Math.max(sellingCurrStock,notSellingCurrStock);
+        }
+    }
+    
+    public int memo(int idx,int[] prices,int[][][] dp,int k,int curr){
+        if(k<0)return Integer.MIN_VALUE;
+        if(idx==prices.length){
+            return dp[idx][k][curr]=0;
+        }
+        if(dp[idx][k][curr]!=-1)return dp[idx][k][curr];
+        
+        if(curr==BUY){
+            int buyingCurrStock=memo(idx+1,prices,dp,k,SELL)-prices[idx];
+            int notBuyingCurrStock=memo(idx+1,prices,dp,k,BUY);
+            dp[idx][k][BUY] = Math.max(buyingCurrStock,notBuyingCurrStock);
+            return dp[idx][k][BUY];
+        }else{
+            int sellingCurrStock=memo(idx+1,prices,dp,k-1,BUY)+prices[idx];
+            int notSellingCurrStock=memo(idx+1,prices,dp,k,SELL);
+            dp[idx][k][SELL] = Math.max(sellingCurrStock,notSellingCurrStock);
+            return dp[idx][k][SELL];
+        }
+    }
+    
 }
