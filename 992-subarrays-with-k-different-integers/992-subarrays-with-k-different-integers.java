ff4666bdbe69a5c5178ef56bfe8b56d290470bdf
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-22 11:51:28 +0530

    Time: 132 ms (15.00%), Space: 73.8 MB (19.43%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return subArrayWithAtmostK(nums,k+1)-subArrayWithAtmostK(nums,k);
+    }
+    
+    public int subArrayWithAtmostK(int[] nums,int k){
+        if(k==0)return 0;
+        int n=nums.length;
+        HashMap<Integer,Integer> elemFreq=new HashMap();
+        int si=0,ei=0,numSubArr=0;
+        
+        for(;ei<n;ei++){
+            elemFreq.put(nums[ei],elemFreq.getOrDefault(nums[ei],0)+1);
+            
+            while(elemFreq.size()==k){
+                elemFreq.put(nums[si],elemFreq.get(nums[si])-1);
+                if(elemFreq.get(nums[si])==0)elemFreq.remove(nums[si]);
+                si++;
+            }
+            
+            numSubArr+=ei-si+1;
+        }
+        return numSubArr;
+    }
+}
