4e07cdc5b094e6389933f03bddd521164a683621
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-22 20:31:03 +0530

    Time: 14 ms (80.06%), Space: 50.5 MB (92.65%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -3,20 +3,20 @@ class Solution {
         return atMostK(nums,k) - atMostK(nums,k-1);
     }
     public int atMostK(int[] nums,int k){
-        HashMap<Integer,Integer> freqMap=new HashMap();
+        // HashMap<Integer,Integer> freqMap=new HashMap();
         
         int si=0,ei=0,niceSubArr=0;
         int odd=0;
         for(;ei<nums.length;ei++){
-            freqMap.put(nums[ei],freqMap.getOrDefault(nums[ei],0)+1);
+            // freqMap.put(nums[ei],freqMap.getOrDefault(nums[ei],0)+1);
             odd+=nums[ei]%2;
             while(odd==k+1){
-                freqMap.put(nums[si],freqMap.get(nums[si])-1);
+                // freqMap.put(nums[si],freqMap.get(nums[si])-1);
                 odd-=nums[si] % 2;
                 
-                if(freqMap.get(nums[si])==0){
-                    freqMap.remove(nums[si]);
-                }
+                // if(freqMap.get(nums[si])==0){
+                //     freqMap.remove(nums[si]);
+                // }
                 si++;
             }
             niceSubArr+=ei-si+1;
