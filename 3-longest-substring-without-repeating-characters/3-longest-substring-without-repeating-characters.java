e12ce8620034d5c152abe177b108d1059bbcf3cc
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-09-22 12:05:40 +0530

    Time: 12 ms (64.54%), Space: 44.9 MB (56.75%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,35 +1,16 @@
 class Solution {
-    
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0)return 0;
-        int maxSum=0;
-        int j=0;
-        List<Character> list=new ArrayList();
-        list.add(s.charAt(j));
-        int sum=1;
-        j++;
-        while(j<s.length()){
-            char ch2=s.charAt(j);
-            
-            if(list.contains(ch2)){
-                int size=list.size();
-                while(true){
-                    if(list.get(0)==ch2){
-                        list.remove(0);
-                        break;
-                    }
-                    list.remove(0); 
-                }
-                list.add(ch2);
-                sum=list.size();
-            }else{
-                list.add(ch2);
-                sum++;
+        HashMap<Character,Integer> idxMap=new HashMap();
+        
+        int si=0,ei=0,maxLen=0;//start idx,end idx,maxLen possible
+        
+        for(;ei<s.length();ei++){
+            if(idxMap.containsKey(s.charAt(ei))){
+                si=Math.max(si,idxMap.get(s.charAt(ei))+1);
             }
-            j++;
-            maxSum=Math.max(sum,maxSum);
+            idxMap.put(s.charAt(ei),ei);
+            maxLen=Math.max(maxLen,ei-si+1);
         }
-        maxSum=Math.max(sum,maxSum);
-        return maxSum;
+        return maxLen;
     }
 }
