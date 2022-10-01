cdc25343cfd3ccd51a486783d1071e1e9aca4c1a
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-01 11:38:22 +0530

    Time: 4 ms (22.71%), Space: 42.9 MB (13.86%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -3,7 +3,25 @@ class Solution {
         if(s.charAt(0)=='0')return 0;
         Integer[] dp=new Integer[s.length()+1];
         
-        return memo(0,s,dp);
+        for(int idx=s.length();idx>=0;idx--){
+            if(idx==s.length()){
+                dp[idx]=1;
+                continue;
+            }
+            int ans=0;
+            int ch=s.charAt(idx)-'0';
+            if(ch>=1 && ch<=9){
+                ans+=dp[idx+1];
+            }
+            if(idx+2<=s.length()){
+                int ch2=Integer.parseInt(s.substring(idx,idx+2));
+                if(ch2>=10 && ch2<=26){
+                    ans+=dp[idx+2];
+                }
+            }
+            dp[idx]=ans;
+        }
+        return dp[0];
     }
     public int recursion(int idx,String s){
         //base case
