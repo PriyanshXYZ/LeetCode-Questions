e0ca5b684b9c1e5d2a0cfa730acca3fa272cb8da
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-02 15:00:52 +0530

    Time: 34 ms (54.77%), Space: 42.8 MB (42.29%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numRollsToTarget(int n, int k, int target) {
         // return recursion(0,n,k,target);
-        Integer[][][] dp=new Integer[n+1][k+1][target+1];
+        Integer[][] dp=new Integer[n+1][target+1];
         return memo(n,k,target,dp);
     }
     // k^n tc
@@ -21,14 +21,14 @@ class Solution {
         return ans;
     }
     int mod=(int)(1e9+7);
-    public int memo(int n,int k,int target,Integer[][][] dp){
+    public int memo(int n,int k,int target,Integer[][] dp){
         if(n==0){
             if(target==0){
-                return dp[n][k][target]=1;
-            }else return dp[n][k][target]=0;
+                return dp[n][target]=1;
+            }else return dp[n][target]=0;
         }
         
-        if(dp[n][k][target]!=null)return dp[n][k][target];
+        if(dp[n][target]!=null)return dp[n][target];
         
         int ans=0;
         for(int i=1;i<=k;i++){
@@ -36,6 +36,6 @@ class Solution {
                 ans=(ans%mod+memo(n-1,k,target-i,dp)%mod)%mod;
             }
         }
-        return dp[n][k][target]=ans%mod;
+        return dp[n][target]=ans%mod;
     }
 }
