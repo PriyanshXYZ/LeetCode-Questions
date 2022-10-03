2cf017bdbd5de506e16c2761f6644547c69bce0f
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-03 18:36:15 +0530

    Time: 2 ms (99.08%), Space: 43 MB (83.68%) - LeetHub

--- 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.java
+++ 786-k-th-smallest-prime-fraction/786-k-th-smallest-prime-fraction.java
@@ -5,35 +5,41 @@ class Solution {
         
         while(lo <= hi){
             double mid=(lo + hi)/2;
-            int p=0;
-            int q=1;
             
-            int amount=0;
-            int j=0;
-            for(int i=0;i<arr.length;i++){
-                while(j<arr.length && arr[i]*1.0/arr[j] > mid){
-                    j++;
-                }
-                
-                if(j==arr.length){
-                    break;
-                }
-                
-                if(arr[i]*1.0/arr[j] > p*1.0/q){
-                    p=arr[i];
-                    q=arr[j];
-                }
-                amount+=(arr.length-j);
-            }
+            int[] count=countPair(arr,mid);//count,nume,deno
             
-            if(amount < k){
+            if(count[0] < k){
                 lo=mid;
-            }else if(amount > k){
+            }else if(count[0] > k){
                 hi=mid;
             }else{
-                return new int[]{p,q};
+                return new int[]{count[1],count[2]};
             }
         }
         return null;
     }
+    public int[] countPair(int[] a,double mid){
+        int p=0;
+        int q=1;
+
+        int amount=0;
+        int j=0;
+        for(int i=0;i<a.length;i++){
+            while(j<a.length && a[i]*1.0/a[j] > mid){
+                j++;
+            }
+
+            if(j==a.length){
+                break;
+            }
+
+            if(a[i]*1.0/a[j] > p*1.0/q){
+                p=a[i];
+                q=a[j];
+            }
+            amount+=(a.length-j);
+        }
+        return new int[]{amount,p,q};
+    }
+    
 }
