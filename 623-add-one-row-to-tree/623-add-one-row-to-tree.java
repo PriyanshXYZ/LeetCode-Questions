3a990f33090ab5c7af555249197d4f4472645de6
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-05 18:35:48 +0530

    Time: 2 ms (22.81%), Space: 45 MB (71.90%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -20,22 +20,45 @@ class Solution {
             temp.left=root;
             return temp;
         }
-        dfs(root,val,1,depth);
+        Queue<TreeNode> q=new ArrayDeque();
+        q.add(root);
+        int curDepth=1;
+        
+        while(q.isEmpty()==false){
+            int size=q.size();
+            int flag=0;
+            while(size-->0){
+                TreeNode rNode=q.remove();
+                if(rNode.left!=null)q.add(rNode.left);
+                if(rNode.right!=null)q.add(rNode.right);
+                if(curDepth+1==depth){
+                    TreeNode left=rNode.left;
+                    TreeNode right=rNode.right;
+                    rNode.left=new TreeNode(val);
+                    rNode.right=new TreeNode(val);
+                    rNode.left.left=left;
+                    rNode.right.right=right;
+                    flag=1;
+                }
+            }
+            if(flag==1)break;
+            curDepth++;
+        }
         return root;
     }
-    public void dfs(TreeNode root,int val,int currDepth,int depth){
-        if(root==null)return;
+//     public void dfs(TreeNode root,int val,int currDepth,int depth){
+//         if(root==null)return;
         
-        if(currDepth+1==depth){
-            TreeNode left=root.left;
-            TreeNode right=root.right;
-            root.left=new TreeNode(val);
-            root.right=new TreeNode(val);
-            root.left.left=left;
-            root.right.right=right;
-            return;
-        }
-        dfs(root.left,val,currDepth+1,depth);
-        dfs(root.right,val,currDepth+1,depth);
-    }
+//         if(currDepth+1==depth){
+//             TreeNode left=root.left;
+//             TreeNode right=root.right;
+//             root.left=new TreeNode(val);
+//             root.right=new TreeNode(val);
+//             root.left.left=left;
+//             root.right.right=right;
+//             return;
+//         }
+//         dfs(root.left,val,currDepth+1,depth);
+//         dfs(root.right,val,currDepth+1,depth);
+//     }
 }
