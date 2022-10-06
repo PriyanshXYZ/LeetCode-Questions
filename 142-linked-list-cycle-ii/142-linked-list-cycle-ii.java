739f01ddb15e98c07ff77f428c5887a72547db70
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-06 22:43:55 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (98.87%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -11,6 +11,32 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        
+        boolean cycle=false;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast){
+                cycle=true;
+                break;
+            }
+        }
+        
+        if(fast==null || fast.next==null)return null;
+        ListNode slow2=head;
+        while(slow!=slow2){
+            slow2=slow2.next;
+            slow=slow.next;
+        }
+        return slow;
+        
+    }
+}
+class Solution1 {
+    //o(n) space complexity
+    public ListNode detectCycle(ListNode head) {
         HashSet<ListNode> set=new HashSet();
         while(head!=null){
             if(set.contains(head))return head;
