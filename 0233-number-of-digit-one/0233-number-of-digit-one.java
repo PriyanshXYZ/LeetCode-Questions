5b0eac20202868e231a221c272ce70af7920e1eb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-17 00:08:37 +0530

    Time: 2 ms (16.70%), Space: 41 MB (35.41%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<Integer> nums;
+    Integer[][][] dp;// pos => place value count=> 1's count tight => 0->no 1->yes
+
+    public int countDigitOne(int n) {
+        nums=new ArrayList();
+        while(n>0){
+            nums.add(n%10);
+            n/=10;
+        }
+        dp=new Integer[10][10][2];
+        
+        Collections.reverse(nums);
+        return digitdp(0,0,1);        
+    }
+    public int digitdp(int pos,int count,int tight){
+        if(pos==nums.size())return count;
+        
+        if(dp[pos][count][tight]!=null){
+            return dp[pos][count][tight];
+        }
+        
+        int ans=0;
+        int limit=tight==1?nums.get(pos):9;
+        
+        for(int val=0;val<=limit;val++){
+            int newCnt=val==1?count+1:count;
+            //the digit at the ith position is smaller than the digit at the ith position of the number the number has already become smaller so we can set the flag as 1
+            boolean newTight=tight==1 && val==limit;
+            ans+=digitdp(pos+1,newCnt ,newTight?1:0);
+        }
+        return dp[pos][count][tight]=ans;
+    }
+}
