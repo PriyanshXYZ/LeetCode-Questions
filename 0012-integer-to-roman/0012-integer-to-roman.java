cd1595daa84e6138665ef0bcb211ab7ad3a7aee8
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-20 12:33:49 +0530

    Time: 23 ms (20.06%), Space: 48.3 MB (11.88%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public String intToRoman(int num) {
+       Map<Integer,String> map=new TreeMap(Collections.reverseOrder());
+        //assigning value in hashmap
+        map.put(1,"I");map.put(4,"IV");map.put(5,"V");map.put(9,"IX");map.put(10,"X");map.put(40,"XL");map.put(50,"L");
+        map.put(90,"XC");map.put(100,"C");map.put(400,"CD");map.put(500,"D");map.put(900,"CM");map.put(1000,"M");
+        // System.out.println(map);
+        StringBuilder sb=new StringBuilder();
+        for(int key:map.keySet()){
+            while(num>=key){
+                sb.append(map.get(key));
+                num-=key;
+            }
+        }
+        return sb.toString();
+    }
+}
