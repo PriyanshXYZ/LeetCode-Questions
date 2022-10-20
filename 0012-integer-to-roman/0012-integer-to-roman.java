500848987322daaab72177211c50b201eabed645
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-20 12:36:20 +0530

    Time: 10 ms (59.13%), Space: 42.2 MB (90.27%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -8,6 +8,7 @@ class Solution {
         // System.out.println(map);
         StringBuilder sb=new StringBuilder();
         for(int key:map.keySet()){
+            if(num==0)break;
             while(num>=key){
                 sb.append(map.get(key));
                 num-=key;
