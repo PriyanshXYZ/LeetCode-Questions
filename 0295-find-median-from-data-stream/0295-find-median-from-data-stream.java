4f08fd760a0538811b017b3169fb7dc6e8abd29f
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-20 23:08:59 +0530

    Time: 207 ms (63.76%), Space: 125.3 MB (39.74%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,8 +1,39 @@
 class MedianFinder {
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
+    public MedianFinder() {
+        left=new PriorityQueue(Collections.reverseOrder());
+        right=new PriorityQueue();
+    }
     
+    public void addNum(int num) {
+        if(left.size()>0 && num<left.peek()){
+            left.add(num);
+        }else{
+            right.add(num);
+        }
+        if(left.size()-right.size()==2){
+            right.add(left.remove());
+        }else if(right.size()-left.size()==2){
+            left.add(right.remove());
+        }
+    }
+    
+    public double findMedian() {
+        if(left.size()>right.size()){
+            return left.peek();
+        }else if(right.size()>left.size()){
+            return right.peek();
+        }else{
+            return (double)(left.peek()+right.peek())/2;
+        }
+    }
+}
+class MedianFinder1 {
+    //worst case O(n^2)..
     List<Integer> nums;
     int size;
-    public MedianFinder() {
+    public MedianFinder1() {
         nums=new ArrayList();
         size=0;
     }
@@ -29,6 +60,13 @@ class MedianFinder {
         return nums.get(size/2);
     }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
