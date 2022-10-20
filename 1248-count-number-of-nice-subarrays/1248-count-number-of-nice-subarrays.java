ebf00ecaf0123d819be9f467ef599ef905d8f903
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-20 23:42:39 +0530

    Time: 113 ms (20.74%), Space: 113 MB (26.93%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,4 +1,21 @@
 class Solution {
+    //alternate way using hashmap of freq...
+    public int numberOfSubarrays(int[] nums, int k) {
+        HashMap<Integer,Integer> freqMap=new HashMap();
+        
+        int res=0;
+        freqMap.put(0,1);
+        int odd=0;
+        for(int i=0;i<nums.length;i++){
+            odd+=nums[i]%2;
+            freqMap.put(odd,freqMap.getOrDefault(odd,0)+1);
+            
+            res+=freqMap.getOrDefault(odd-k,0);
+        }
+        return res;
+    }
+}
+class Solution1 {
     public int numberOfSubarrays(int[] nums, int k) {
         return atMostK(nums,k) - atMostK(nums,k-1);
     }
@@ -11,14 +28,12 @@ class Solution {
             
             odd+=nums[ei]%2;
             while(odd==k+1){
-        
                 odd-=nums[si] % 2;
-                
-        
                 si++;
             }
             niceSubArr+=ei-si+1;
         }
         return niceSubArr;
     }
+    
 }
