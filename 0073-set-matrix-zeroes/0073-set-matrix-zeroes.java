8bc7481463f2a10556aea048cff0f8c043e252f3
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-22 15:26:37 +0530

    Time: 1 ms (97.43%), Space: 54.6 MB (8.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,4 +1,45 @@
 class Solution {
+    public void setZeroes(int[][] a) {
+        int n=a.length;
+        int m=a[0].length;
+        
+        boolean col0=false;
+        
+        for(int i=0;i<n;i++){
+            if(a[i][0]==0){
+                col0=true;
+            }
+            for(int j=1;j<m;j++){
+                if(a[i][j]==0){
+                    a[0][j]=0;
+                    a[i][0]=0;
+                }
+            }
+        }
+        
+        //iterating thorugh cols
+        for(int j=1;j<m;j++){
+            for(int i=1;i<n;i++){
+                if(a[0][j]==0 || a[i][0]==0){
+                    a[i][j]=0;
+                }
+            }
+        }
+        
+        if(a[0][0]==0){
+            for(int j=0;j<m;j++){
+                a[0][j]=0;
+            }    
+        }
+        if(col0){
+            for(int i=0;i<n;i++){
+                a[i][0]=0;
+            }
+        }
+        
+    }
+}
+class Solution1 {
     class Pair{
         int x;
         int y;
@@ -8,6 +49,7 @@ class Solution {
             this.y=y;
         }
     }
+    //time complexity O(n*m)  space compelxty O(zeros)
     public void setZeroes(int[][] matrix) {
         List<Pair> zeros=new ArrayList();
         int n=matrix.length;
@@ -34,4 +76,5 @@ class Solution {
             }
         }
     }
+    
 }
