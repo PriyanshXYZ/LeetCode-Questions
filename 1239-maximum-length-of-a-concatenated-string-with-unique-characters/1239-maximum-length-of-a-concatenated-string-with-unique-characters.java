d9ac6c1bcf943d43f520d9ba83d5d49aa98d2bfb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-25 10:47:10 +0530

    Time: 1109 ms (5.07%), Space: 144.1 MB (6.48%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,23 +1,25 @@
 class Solution {
     public int maxLength(List<String> arr) {
-        return recursion(0,arr.size(),"",arr);
+        return recursion(0,arr.size(),new StringBuilder(),arr);
     }
     
-    public int recursion(int idx,int n,String s,List<String> a){
+   public int recursion(int idx,int n,StringBuilder sb,List<String> a){
         if(idx==n){
             HashSet<Character> set=new HashSet();
-            for(char ch:s.toCharArray()){
+            for(char ch:sb.toString().toCharArray()){
                 if(!set.add(ch)){
                     return 0;
                 }
             }
-            
-            return s.length();
+            return sb.length();
         }
-        
-        int inc=recursion(idx+1,n,s+a.get(idx),a);
-        
-        int exc=recursion(idx+1,n,s,a);
+        sb.append(a.get(idx));
+        int inc=recursion(idx+1,n,sb,a);
+        int size=a.get(idx).length();
+        while(size-->0){
+            sb.deleteCharAt(sb.length()-1);
+        }
+        int exc=recursion(idx+1,n,sb,a);
         
         return Math.max(inc,exc);
     }
