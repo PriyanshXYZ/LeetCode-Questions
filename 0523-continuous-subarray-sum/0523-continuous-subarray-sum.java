9c4cf1f583e29bd9245003edb7a52a9d12af8844
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-26 11:13:59 +0530

    Time: 21 ms (90.86%), Space: 60.4 MB (86.00%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        int[] prefix=new int[nums.length];
-        prefix[0]=nums[0]%k;
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=(prefix[i-1]+nums[i]);
-        }
-        
         HashMap<Integer,Integer> map=new HashMap();
+        int sum=0;
         map.put(0,-1); //put this as if there exist a subarray from start ,we should be able to find it .. eg. 23, 2, 4, 6 ,6
         for(int i=0;i<nums.length;i++){
-            int rem=prefix[i]%k;
+            sum+=nums[i];
+            int rem=sum%k;
             if(!map.containsKey(rem)){
                 map.put(rem,i);    
             }else if((i-map.get(rem))>1)return true;
