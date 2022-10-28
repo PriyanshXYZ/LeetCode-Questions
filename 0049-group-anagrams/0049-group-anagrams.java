ef7eded421d139851b4abd535c709754e0457789
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-28 10:54:09 +0530

    Time: 184 ms (5.01%), Space: 70.1 MB (7.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length == 0) return Collections.emptyList();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            int[] frequencyArr = new int[128];   
+            for(int i = 0;i<s.length();i++){
+                frequencyArr[s.charAt(i) - 'a']++;
+            }
+            String key = Arrays.toString(frequencyArr);
+            List<String> tempList = map.getOrDefault(key, new LinkedList<String>());
+            tempList.add(s);
+            map.put(key,tempList);
+        }
+        // System.out.println(map);
+        return new LinkedList<>(map.values());
+    }
+}
+class Solution1 {
+    public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map=new HashMap();
         
         for(String str:strs){
