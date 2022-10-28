1303227549203f81b55113fa8b49f378ac2fee47
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-28 10:56:32 +0530

    Time: 20 ms (32.22%), Space: 45.8 MB (88.61%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -18,6 +18,9 @@ class Solution1 {
 }
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+         if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
         HashMap<String,List<String>> map=new HashMap();
         
         for(String str:strs){
