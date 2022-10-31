1ee5b64e240e3d07e6868689cc92c3ff279b9a5f
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-10-31 20:04:50 +0530

    Time: 321 ms (42.44%), Space: 128.4 MB (56.83%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -13,46 +13,41 @@ class Solution {
             return this.obs-o.obs;
         }
     }
+    int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
     public int minimumObstacles(int[][] grid) {
         PriorityQueue<Pair> q=new PriorityQueue();
         int[][] vis=new int[grid.length][grid[0].length];
         
-        for(int i=0;i<vis.length;i++)Arrays.fill(vis[i],-1);
-        // int minObs=Integer.MAX_VALUE;
+        for(int i=0;i<vis.length;i++)Arrays.fill(vis[i],Integer.MAX_VALUE);
+        
         q.add(new Pair(0,0,0));
+        vis[0][0]=grid[0][0];
         while(q.size()!=0){
-            // System.out.println(q);
             Pair rem=q.remove();
             int x=rem.x;
             int y=rem.y;
-            
-            //invalid cell
-            // if(x<0 || y<0 || x>=grid.length || y>=grid[0].length){
-            //     continue;
-            // }
-            if(grid[x][y]==1){
-                rem.obs+=1;
-            }
+        
             //we found our cell
             if(x==grid.length-1 && y==grid[0].length-1){
-                // minObs=Math.min(minObs,rem.obs);
                 return rem.obs;
             }
             
-            if(vis[x][y]!=-1 && vis[x][y]<=rem.obs){
-                continue;
-            }
-            vis[x][y]=rem.obs;
-            
             //add neigbours to queue
-            if(x+1<grid.length)
-                q.add(new Pair(x+1,y,rem.obs));
-            if(x>0)
-                q.add(new Pair(x-1,y,rem.obs));
-            if(y+1<grid[0].length)
-                q.add(new Pair(x,y+1,rem.obs));
-            if(y>0)
-                q.add(new Pair(x,y-1,rem.obs));
+            for(int[] dir:dirs){
+                int xx=x+dir[0];
+                int yy=y+dir[1];
+                // invalid cell
+                if(xx<0 || yy<0 || xx>=grid.length || yy>=grid[0].length){
+                    continue;
+                }
+                
+                if(rem.obs+grid[xx][yy]<vis[xx][yy]){
+                        
+                    q.add(new Pair(xx,yy,rem.obs+grid[xx][yy]));
+                    vis[xx][yy]=rem.obs+grid[xx][yy];
+                }
+                
+            }
         }
         return -1;
     }
