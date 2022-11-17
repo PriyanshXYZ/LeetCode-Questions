bde7026867d40842499974c85282b7ffb3d32beb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-11-17 18:58:20 +0530

    Time: 5 ms (51.55%), Space: 43 MB (51.90%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -4,7 +4,6 @@ class Solution {
         int rect2=Math.abs(by1-by2)*Math.abs(bx1-bx2);
         
         //when is merging possible
-        // start y point of second rectangle is lesser than end y of first array
         int rect3=0;
         if(check(ax1,ay1,ax2,ay2,bx1,by1,bx2,by2)){
             int xlen=Math.abs(Math.min(ax2,bx2)-Math.max(ax1,bx1));
@@ -13,7 +12,6 @@ class Solution {
         }
         return rect1+rect2-rect3;
     }
-//     bx1<=ax2 && ax1<=bx1 && by1<=ay2 && by1>=ay1 
     private boolean check(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2){
         //if starting of second rectangle is greater than ending of first
         //if ending of second is lesser than starting of first
