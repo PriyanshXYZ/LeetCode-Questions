e9c1c307d309df565efd74cacf698fbac0290b5a
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-11-20 10:54:00 +0530

    Time: 2550 ms (14.29%), Space: 54.7 MB (28.57%) - LeetHub

--- 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.java
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.java
@@ -1,32 +1,3 @@
-class Solution1 {
-    public int beautifulPartitions(String s, int k, int minLength) {
-        if (k * minLength > s.length() || !isPrime(s.charAt(0)) || isPrime(s.charAt(s.length() - 1))) {
-            return 0;
-        }
-        int[][] dp = new int[s.length() + 1][k + 1];
-        dp[0][0] = 1;
-        for (int i = 0; i < s.length() - minLength + 1; i++) {
-            if (isPrime(s.charAt(i)) && (i == 0 || !isPrime(s.charAt(i - 1)))) {
-                for (int j = i + minLength - 1; j < s.length(); j++) {
-                    if (!isPrime(s.charAt(j))) {
-                        for (int p = 0; p < k; p++) {
-                            dp[j + 1][p + 1] += dp[i][p];
-                            dp[j + 1][p + 1] %= 1_000_000_007;
-                        }
-                    }
-                }
-            }
-        }
-        return dp[s.length()][k];
-    }
-
-    private boolean isPrime(char c) {
-        if (c == '2' || c == '3' || c == '5' || c == '7') {
-            return true;
-        }
-        return false;
-    }
-}
 class Solution {
     
     public int beautifulPartitions(String s, int k, int minLen) {
