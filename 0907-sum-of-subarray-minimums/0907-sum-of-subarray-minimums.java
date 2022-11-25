f7e917610da25b6f3f7cced93aab69a90ffc7c5e
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-11-25 16:59:54 +0530

    Time: 229 ms (5.14%), Space: 73.1 MB (16.49%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int[] nsl=nsl(arr);
+        int[] nsr=nsr(arr);
+        
+        int mod=(int)(1e9+7);
+        
+        long ans=0;
+        for(int i=0;i<arr.length;i++){
+            long temp=(long)((i-nsl[i])*(nsr[i]-i))%mod;
+            ans=(ans%mod+(temp*arr[i])%mod)%mod;
+        }
+        // System.out.println(ans);
+        return (int)ans;
+    }
+    
+    public int[] nsl(int[] arr){
+        int n=arr.length;
+        int[] res=new int[n];
+        Arrays.fill(res,-1);
+        Stack<Integer> stk=new Stack();
+        
+        for(int i=0;i<n;i++){
+            while(stk.size()>0 && arr[stk.peek()]>=arr[i]){
+                stk.pop();
+            }
+            if(stk.size()>0){
+                res[i]=stk.peek();
+            }
+            stk.push(i);
+        }
+        // for(int i=0;i<n;i++){
+        //     System.out.print(res[i]+" ");
+        // }
+        // System.out.println();
+        return res;
+    }
+    
+    public int[] nsr(int[] arr){
+        int n=arr.length;
+        int[] res=new int[n];
+        Arrays.fill(res,n);
+        Stack<Integer> stk=new Stack();
+        
+        for(int i=n-1;i>=0;i--){
+            while(stk.size()>0 && arr[stk.peek()]>arr[i]){
+                stk.pop();
+            }
+            if(stk.size()>0){
+                res[i]=stk.peek();
+            }
+            stk.push(i);
+        }
+        // for(int i=0;i<n;i++){
+        //     System.out.print(res[i]+" ");
+        // }
+        // System.out.println();
+        return res;
+    }
+}
