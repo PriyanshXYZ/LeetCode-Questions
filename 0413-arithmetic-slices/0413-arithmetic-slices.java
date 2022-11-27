a26b35a982dbd0b6f87a4789d7eb125fd16a3145
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-11-27 22:37:33 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (61.27%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -1,5 +1,22 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
+        int sum=0,curr=0;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                curr+=1;
+                
+                sum+=curr;
+            }else{
+                curr=0;
+            }
+        }
+        
+        return sum;
+    }
+}
+class Solution1 {
+    //O(n^2)
+    public int numberOfArithmeticSlices(int[] nums) {
         int sum=0;
         for(int i=1;i<nums.length;i++){
             int count=0;
