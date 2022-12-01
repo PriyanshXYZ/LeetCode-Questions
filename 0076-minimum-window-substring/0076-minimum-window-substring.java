4ceca75775af1965deb3cba5af34711643430f81
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-12-01 12:53:38 +0530

    Time: 17 ms (66.31%), Space: 42.8 MB (89.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -14,32 +14,30 @@ class Solution {
         int minLeft = 0;
         int minLen = s.length()+1;
         int count = 0;
-        for(int right = 0; right < s.length(); right++){
+        for(int right=0;right<s.length();right++){
             if(map.containsKey(s.charAt(right))){
                 map.put(s.charAt(right),map.get(s.charAt(right))-1);
-                if(map.get(s.charAt(right)) >= 0){
-                    count ++;
+                if(map.get(s.charAt(right))>=0){
+                    count++; // increase count if there is a character from t string
                 }
-                while(count == t.length()){
-                    if(right-left+1 < minLen){
-                        minLeft = left;
-                        minLen = right-left+1;
+                //when we reach a point where there is count=length of t
+                while(count==t.length()){
+                    if(right-left+1<minLen){
+                        minLeft=left;
+                        minLen=right-left+1;
                     }
+                    
                     if(map.containsKey(s.charAt(left))){
                         map.put(s.charAt(left),map.get(s.charAt(left))+1);
-                        if(map.get(s.charAt(left)) > 0){
-                            count --;
+                        if(map.get(s.charAt(left))>0){//valid character which affects the ans
+                            count--;
                         }
                     }
-                    left ++ ;
+                    left++;
                 }
             }
         }
-        if(minLen>s.length())  
-        {  
-            return "";  
-        }  
-
+        if(minLen>s.length())return "";
         return s.substring(minLeft,minLeft+minLen);
     }
 }
