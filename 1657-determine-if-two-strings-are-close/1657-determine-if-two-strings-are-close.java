54be3ebfd10043e1a0f53f1557339926c997bc06
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-12-02 11:34:58 +0530

    Time: 26 ms (64.89%), Space: 43.7 MB (79.11%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -6,20 +6,19 @@ class Solution {
         Integer[] f2=new Integer[26];
         Arrays.fill(f2,0);
         
-        HashSet<Character> s1=new HashSet();
         for(char ch:word1.toCharArray()){
             f1[ch-'a']++;
-            s1.add(ch);
         }
-        Arrays.sort(f1,Collections.reverseOrder());
         
         for(char ch:word2.toCharArray()){
             f2[ch-'a']++;
-            if(!s1.contains(ch)){
-                // System.out.println("Hashset k karan");
-                return false;
-            }
         }
+        
+        for(int i=0;i<26;i++){
+            if(f1[i].equals(f2[i]))continue;
+            if(f1[i]==0 || f2[i]==0)return false;
+        }
+        Arrays.sort(f1,Collections.reverseOrder());
         Arrays.sort(f2,Collections.reverseOrder());
         
         for(int i=0;i<26;i++){
