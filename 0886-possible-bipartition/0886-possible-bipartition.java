3351fefdba29a9e6f76dd9fa1ea70c3db835c85a
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-12-21 15:01:03 +0530

    Time: 349 ms (5.00%), Space: 153.9 MB (5.00%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        //bipartite is only possible when 
+        //1. when there is no cycle present
+        //2. if cycle is present it must be even in color.
+        
+        List<List<Integer>> graph = new ArrayList();
+        for(int i=0;i<=n;i++)graph.add(new ArrayList());
+        
+        for(int[] edge : dislikes){
+            int u = edge[0];
+            int v = edge[1];
+            
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        
+        int[] vis = new int[n + 1];
+        Arrays.fill(vis, -1);
+        for(int vtc = 1; vtc <=n; vtc++){
+            if(vis[vtc] == -1){
+                if(hasOddCycle(vtc, graph, vis))return false;
+            }
+        }
+        return true;
+    }
+    class Pair{
+        int node;
+        int len;
+        Pair(int node, int len){
+            this.node = node;
+            this.len = len;
+        }
+    }
+    public boolean hasOddCycle(int src, List<List<Integer>> graph, int[] vis){
+        Queue<Pair> q = new ArrayDeque();
+        q.add(new Pair(src, 0));
+        
+        while(q.size()>0){
+            Pair rem = q.remove();
+            if(vis[rem.node]!=-1){
+                //cycle is present and now we need need to check the length of the cycle
+                if((vis[rem.node] + rem.len) % 2 == 1)return true;
+            }
+            vis[rem.node] = rem.len;
+            for(int nbr : graph.get(rem.node)){
+                if(vis[nbr]==-1)
+                    q.add(new Pair(nbr,rem.len + 1));
+            }
+        }
+        return false;
+    }
+}
