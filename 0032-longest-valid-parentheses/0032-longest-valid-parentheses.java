eb979aed5abf0bb83fdcfb90300509e6755e97ee
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-12-21 12:31:59 +0530

    Time: 4 ms (71.64%), Space: 43.1 MB (53.57%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,4 +1,35 @@
 class Solution {
+    public int longestValidParentheses(String s) {
+        Integer[] dp = new Integer[s.length()+1];
+        int res =  0;
+        for(int i = 0; i < s.length(); i++){
+            res = Math.max(res, memo(i, s, dp));
+        }
+        return res;
+    }
+    private int memo(int idx, String s, Integer[] dp){
+        if(idx < 0)return 0;
+        
+        if(dp[idx] != null)return dp[idx];
+        
+        int ans = 0;
+        if(s.charAt(idx)==')'){
+            if(idx > 0 && s.charAt(idx - 1) == '('){
+                ans += 2;
+                ans += memo(idx - 2, s, dp);
+            }else if(idx > 0 && s.charAt(idx - 1) == ')'){
+                int prevLen = memo(idx - 1, s, dp); // idx-1 has valid parentheses?
+                if(prevLen > 0 && idx - prevLen - 1 >= 0 && s.charAt(idx- prevLen - 1) == '('){
+                    ans += 2;
+                    ans += prevLen;
+                    ans += memo(idx - prevLen - 2, s, dp);//searching for previous answer
+                }
+            }
+        }
+        return dp[idx] = ans;
+    }
+}
+class Solution1 {
 
     public int longestValidParentheses(String s) {
         Stack<Integer> stk=new Stack();
@@ -17,7 +48,6 @@ class Solution {
                 }
             }
         }
-        
         return count;
     }
 }
