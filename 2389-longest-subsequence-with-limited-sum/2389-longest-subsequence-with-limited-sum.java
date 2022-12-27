994cf3f127f9548589a5481d27238e6c4b4e95c3
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2022-12-27 09:38:50 +0530

    Time: 5 ms (89.06%), Space: 42.9 MB (76.68%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -6,9 +6,24 @@ class Solution {
         for (int i = 1; i < n; ++i)
             A[i] += A[i - 1];
         for (int i = 0; i < m; ++i) {
-            int j = Arrays.binarySearch(A, queries[i]);
-            res[i] = Math.abs(j + 1);
+            int j = binarySearch(A, queries[i]);
+            res[i] = j;
         }
         return res;
+    }
+    int binarySearch(int[] a, int val){
+        int lo = 0, hi = a.length - 1;
+        
+        while(lo < hi){
+            int mid = (lo + hi) / 2;
+            if(a[mid] == val){
+                return mid + 1;
+            }else if(a[mid] < val){
+                lo = mid + 1;
+            }else{
+                hi = mid - 1;
+            }
+        }
+        return a[lo] > val ? lo : lo + 1;
     }
 }
