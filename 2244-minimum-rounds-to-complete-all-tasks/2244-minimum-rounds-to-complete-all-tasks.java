fd762ae37589166ef5ce92cf9728a4ddeb0e5eae
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-04 08:12:53 +0530

    Time: 117 ms (12.52%), Space: 108.5 MB (18.09%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        Arrays.sort(tasks);
+        Map<Integer,Integer> map = new HashMap();
+        for(int i=0;i<tasks.length;i++){
+            map.put(tasks[i], map.getOrDefault(tasks[i],0)+1);
+        }
+        System.out.println(map);
+        int minRounds = 0;
+        for(int value : map.values()){
+            if(value==1)return -1;
+            if(value %3 == 0){
+                minRounds += value/3;
+                
+            }else{
+                minRounds += value/3 + 1; //remainder can be 1 or 2
+                //for 1 take example 7 so we made group of 3 3 1 (invalid) which can be done as 3 2 2 
+                //for 2 take example 8 we can directly add 1 of 2 task.
+            }
+            
+           
+        }
+        return minRounds;
+    }
+   
+}
