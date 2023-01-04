70bee421f72366242a2f5580f355ab15f723c456
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-04 23:12:26 +0530

    Added solution - LeetHub

---
+++ Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntMatrix {
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException {
+        int[][] mat = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for (int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m) {
+        for (var a : m) {
+            for (int e : a) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m) {
+        for (var a : m) {
+            for (int e : a) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[][] intervals = IntMatrix.input(br, n, 3);
+
+            Solution obj = new Solution();
+            int res = obj.maximum_profit(n, intervals);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int maximum_profit(int n, int[][] intervals) {
+        // code here
+        Arrays.sort(intervals,(a,b)->(a[1]-b[1]));
+        
+        TreeMap<Integer,Integer> dp=new TreeMap<>();
+        dp.put(0,0);
+        for(int[] interval:intervals){
+            int curr=dp.floorEntry(interval[0]).getValue()+interval[2];//logn operation
+            if(curr>dp.lastEntry().getValue()){
+                dp.put(interval[1],curr);
+            }
+        }
+        return dp.lastEntry().getValue();
+    }
+}
