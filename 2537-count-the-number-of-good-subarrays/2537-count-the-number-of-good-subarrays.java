4521dfbb4dff5d41ba4ca68331b826a87bde5ab1
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-15 11:07:51 +0530

    Time: 72 ms (100.00%), Space: 59 MB (100.00%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
@@ -15,12 +15,43 @@ class Solution {
                 if(map.getOrDefault(nums[strt],0)>1){
                     pairCnt-=(map.get(nums[strt])-1);
                 }
-                count += n - end;
+                
                 map.put(nums[strt],map.getOrDefault(nums[strt],0)-1);
                 strt++;
             }
+            // so basically we are moving j until we get subarray smaller than j , so when we reach curr<k so adding all previous elements and we would still get our valid good subarray
+            count += strt;
         }
         
         return count;
     }
 }
+/*
+It is sliding window because we are going over range i to j and we are dealing with that range for our answer and not thinking about other part of the array
+like for eg take => nums = [3,1,4,3,2,2,4], k = 2 
+```
+//curr describes about how many pairs do we have till now
+i=0,j=0   [3]        curr=0
+i=1,j=0   [3,1]        curr =0
+i=2,j=0   [3,1,4]      curr =0
+i=3,j=0   [3,1,4,3]   (here we got our first pair) curr=1
+i=4,j=0   [3,1,4,3,2]    curr=1
+i=5,j=0   [3,1,4,3,2,2]  curr =2 our condition is satisfied that we got curr = k
+                         now we need to increment j pointer until we do not get curr < k
+                         i=6,j=0                       [3,1,4,3,2,2]
+                         i=6,j=1                       [1,4,3,2,2]                  here our condition is violated so we get out of loop
+                         curr = curr - (elements which were in pair with curr removed element)
+         it means we got 1 subarray satisfying the condition ie.[3,1,4,3,2,2]
+res +=1
+i=6,j=1  [1,4,3,2,2,4]      curr =2 = k
+                       increment j pointer until we do not get curr < k
+                        i=6,j=1                         [1,4,3,2,2,4] 
+                        i=6,j=2                         [4,3,2,2,4]        still curr is 2
+                       i=6,j=3                          [3,2,2,4]              curr=1
+so we got 3 subarray which satiesfies >=k that are           [1,4,3,2,2,4] 
+                                                                                         [3,1,4,3,2,2,4]
+                                                                                         [4,3,2,2,4]
+res+=3 (which is j)
+```
+
+*/
