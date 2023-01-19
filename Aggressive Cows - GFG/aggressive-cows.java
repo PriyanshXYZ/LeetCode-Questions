a4dd62971e6955aaa62160fc0375a6569595dee9
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-19 22:57:37 +0530

    Added solution - LeetHub

---
+++ Aggressive Cows - GFG/aggressive-cows.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n;
+            n = sc.nextInt();
+
+            int k;
+            k = sc.nextInt();
+
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++) v[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.solve(n, k, v);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int solve(int n, int k, int[] stalls) {
+        Arrays.sort(stalls);
+        
+        int lo = 0;
+        int hi = stalls[n-1] - stalls[0];
+        
+        int ans = -1;
+        while(lo<=hi){
+            int minDist = lo + (hi - lo)/2;
+            
+            int aggresiveCows = 1;
+            int lastPlacedCow = stalls[0];
+            for(int i = 1; i<n;i++){
+                if(stalls[i] - lastPlacedCow >= minDist){
+                    aggresiveCows++;
+                    lastPlacedCow = stalls[i];
+                }
+            }
+            
+            if(aggresiveCows >= k){
+                //more agressive cows we need to increse distance
+                ans = minDist;
+                lo = minDist + 1;
+            }else{
+                //less agressive cows we need to check if for more min distance can we get the lesser cows
+                
+                hi = minDist - 1;
+            }
+        }
+        return ans;
+    }
+}
