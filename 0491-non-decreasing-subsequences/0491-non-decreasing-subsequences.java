8eec21630e0c9ad67dd175620882871e31eaa83d
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-20 11:46:04 +0530

    Time: 3 ms (100.00%), Space: 47.9 MB (95.82%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -3,23 +3,23 @@ class Solution {
     List<List<Integer>> res;
     public List<List<Integer>> findSubsequences(int[] nums) {
         res = new ArrayList();
-        recursion(0,nums,new ArrayList());
+        recursion(0,nums,-101,new ArrayList());
         return res;
     }
-    public void recursion(int idx, int[] nums , List<Integer> subseq){
+    public void recursion(int idx, int[] nums , int prev, List<Integer> subseq){
         if(idx==nums.length){
             if(subseq.size()>1)res.add(new ArrayList(subseq));
             return;
         }
         
-        if(subseq.size()==0 || subseq.get(subseq.size()-1)<=nums[idx]){
+        if(prev<=nums[idx]){
             subseq.add(nums[idx]);
             
-            recursion(idx+1,nums,subseq);
+            recursion(idx + 1 , nums , nums[idx] ,subseq);
             
             subseq.remove(subseq.size()-1);
         }
-        if(idx>0 && subseq.size()>0 && subseq.get(subseq.size()-1)==nums[idx])return;
-        recursion(idx+1,nums,subseq);
+        if(prev==nums[idx])return;
+        recursion(idx + 1 , nums , prev , subseq);
     }
 }
