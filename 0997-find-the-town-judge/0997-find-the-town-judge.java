7fb7dacab3b84186c5ce85b3481cbb8f83daa3ba
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-23 11:54:55 +0530

    Time: 4 ms (55.79%), Space: 70.3 MB (30.07%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,5 +1,23 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        int[] score = new int[n+1];
+        
+        for(int[] t : trust){
+            int a = t[0];
+            int b = t[1];
+            
+            score[a]--;
+            score[b]++;
+        }
+        
+        for(int i = 1 ;i<=n;i++){
+            if(score[i]==n-1)return i;
+        }
+        return -1;
+    }
+}
+class Solution1 {
+    public int findJudge(int n, int[][] trust) {
         if(n==1)return 1;
         HashMap<Integer,Integer> trustedBy = new HashMap<>();
         HashMap<Integer,Integer> trusts = new HashMap<>();
