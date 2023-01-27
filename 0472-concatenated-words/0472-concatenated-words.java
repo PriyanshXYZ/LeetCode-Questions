ee64520ca0e3b363f1529c4766fdda206252d6d3
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-01-28 00:27:25 +0530

    Time: 160 ms (18.65%), Space: 118.7 MB (5.14%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,4 +1,40 @@
 class Solution {
+    //can be solved with dp as well
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Set<String> wordSet = new HashSet<>(Arrays.asList(words));
+        List<String> res = new ArrayList<>();
+        
+        for(String word : words){
+            Boolean[] dp = new Boolean[word.length()+1];
+            wordSet.remove(word);
+            if(memo(0, word, wordSet,dp)){
+                res.add(word);
+            }else wordSet.add(word);
+        }
+        return res;
+    }
+    
+    public boolean memo(int strtIdx, String word, Set<String> wordSet, Boolean[] dp){
+        if(wordSet.size()==0)return false;
+        
+        if(strtIdx ==  word.length())return true;
+        
+        if(dp[strtIdx] != null)return dp[strtIdx];
+        
+        for(int endIdx = strtIdx + 1 ; endIdx <= word.length() ; endIdx++){
+            String prefix =word.substring(strtIdx,endIdx);
+            String suffix = word.substring(endIdx);
+            if((wordSet.contains(prefix) && wordSet.contains(suffix)) || 
+              (wordSet.contains(prefix) && memo(endIdx , word , wordSet , dp))){
+                dp[strtIdx]=true;
+                return true;
+            }
+        }
+        dp[strtIdx]=false;
+        return false;
+    }
+}
+class Solution1 {
     class Trie{
         boolean isEnd = false;
         Trie[] child = new Trie[26];
