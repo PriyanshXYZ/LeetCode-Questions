050168bedba406a63a3e2b5c0576c4d303de6007
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-06 14:00:51 +0530

    Time: 4 ms (99.39%), Space: 43.5 MB (58.65%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,18 +2,34 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         if(p.length() > s.length())return new ArrayList();
         List<Integer> res = new ArrayList();
-        char[] arr =p.toCharArray();
-        Arrays.sort(arr);
-        p = String.valueOf(arr);
-        // System.out.println(p);
-        for(int i=p.length();i<=s.length();i++){
-            String substr = s.substring(i-p.length(),i);
-            arr = substr.toCharArray();
-            Arrays.sort(arr);
-            String a = String.valueOf(arr);
-            // System.out.println(a);
-            if(a.equals(p))res.add(i-p.length());
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(char ch : p.toCharArray()){
+            freq1[ch-'a']++;
         }
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            freq2[ch-'a']++;
+            
+            if(i >= p.length()-1){
+                if(i-p.length()>=0){
+                    char ch1 = s.charAt(i-p.length());    
+                    freq2[ch1-'a']--;
+                }        
+                // System.out.print(ch+" ");
+                
+                boolean flag = true;
+                for(int j=0;j<26;j++){
+                    if(freq1[j]!=freq2[j]){
+                        flag=false;
+                        // System.out.println((char)(j+'a'));
+                        break;
+                    }
+                }  
+                if(flag)res.add(i-p.length()+1);
+            }
+        }
+        
         return res;
     }
 }
