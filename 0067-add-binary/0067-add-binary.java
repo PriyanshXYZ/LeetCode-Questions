543b577761f71ec1d94eae44589dcee7a50a497c
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-14 10:42:40 +0530

    Time: 7 ms (19.54%), Space: 42.5 MB (31.98%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        boolean carry = false;
+        
+        int idx1 = a.length() - 1, idx2 = b.length() - 1;
+        String res = "";
+        while(idx1>=0 || idx2>=0 || carry==true){
+            int val1 = idx1>=0?a.charAt(idx1)-'0':0;
+            int val2 = idx2>=0?b.charAt(idx2)-'0':0;
+            
+            int sum = val1+val2 + (carry?1:0);
+            int rem  = sum % 2;
+            
+            res = rem + "" + res;
+            if(sum/2!=0){
+                carry =true;
+            }else{
+                carry =false;
+            }
+            idx1--;
+            idx2--;
+        }
+        return res;
+    }
+}
