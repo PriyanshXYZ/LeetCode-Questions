9f1bee9ce16e82f02dc6c2471a3cd8f44261df2e
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-16 10:08:37 +0530

    Time: 7 ms (22.18%), Space: 43.1 MB (23.94%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -6,8 +6,13 @@ class Solution {
             String s2 = words[i];
             char[] ch1 = s1.toCharArray();
             char[] ch2 = s2.toCharArray();
+            if(ch1.length!=ch2.length){
+                list.add(words[i]);
+                continue;
+            }
             Arrays.sort(ch1);
             Arrays.sort(ch2);
+            
             s1=Arrays.toString(ch1);
             s2=Arrays.toString(ch2);
             if(!s1.equals(s2)){
