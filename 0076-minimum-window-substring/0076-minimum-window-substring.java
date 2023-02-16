31ce0f6dfd653d3504806a031482b678c98ba092
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-16 19:30:19 +0530

    Time: 5 ms (86.98%), Space: 42.6 MB (79.68%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,43 +1,39 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        if(t.length()>s.length())return "";
-        
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-        for(char c : t.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c,map.get(c)+1);
-            }else{
-                map.put(c,1);
-            }
-        }
-        int left = 0;
-        int minLeft = 0;
-        int minLen = s.length()+1;
-        int count = 0;
-        for(int right=0;right<s.length();right++){
-            if(map.containsKey(s.charAt(right))){
-                map.put(s.charAt(right),map.get(s.charAt(right))-1);
-                if(map.get(s.charAt(right))>=0){
-                    count++; // increase count if there is a character from t string
-                }
-                //when we reach a point where there is count=length of t
-                while(count==t.length()){
-                    if(right-left+1<minLen){
-                        minLeft=left;
-                        minLen=right-left+1;
-                    }
-                    
-                    if(map.containsKey(s.charAt(left))){
-                        map.put(s.charAt(left),map.get(s.charAt(left))+1);
-                        if(map.get(s.charAt(left))>0){//valid character which affects the ans
-                            count--;
-                        }
-                    }
-                    left++;
-                }
-            }
-        }
-        if(minLen>s.length())return "";
-        return s.substring(minLeft,minLeft+minLen);
+    public String minWindow(String a, String b) {
+        if(b.length()>a.length())return "";
+		
+		int[] freqB = new int[256];
+		int[] freqA = new int[256];
+		
+		for(char ch : b.toCharArray()){
+			freqB[ch]++;
+		}
+		
+		int minLen = Integer.MAX_VALUE;
+		int strtIdx = -1;
+		int cnt=0;
+		for(int ei=0,si=0;ei<a.length();ei++){
+			int ch1 =a.charAt(ei);
+			freqA[ch1]++;
+			if(freqA[ch1]<=freqB[ch1]){
+				cnt++;
+			}
+			if(cnt==b.length()){
+                //continue to shrink the subarray until we dont delete crucial element
+				while(freqA[a.charAt(si)] > freqB[a.charAt(si)] || freqB[a.charAt(si)]==0){
+					
+					if(freqA[a.charAt(si)] > freqB[a.charAt(si)]){
+						freqA[a.charAt(si)]--;
+					}
+					si++;
+				}
+				if(minLen > ei - si + 1){
+					minLen = ei - si + 1;
+					strtIdx = si;
+				}
+			}
+		}
+		if(strtIdx==-1)return "";
+		else return a.substring(strtIdx,strtIdx+minLen);
     }
 }
