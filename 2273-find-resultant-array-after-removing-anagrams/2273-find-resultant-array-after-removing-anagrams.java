6952a11d6d157bc728a689cb972bfaf68698b805
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-16 10:07:18 +0530

    Time: 7 ms (22.18%), Space: 43 MB (33.33%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        Stack<String> stk = new Stack<>();
-        
-        for(int i=0;i<words.length;i++){
-            if(stk.isEmpty()){
-                stk.push(words[i]);
-            }else{
-                while(i<words.length && isAnagram(stk.peek(),words[i])){
-                    i++;
-                }
-                if(i<words.length)
-                    stk.push(words[i]);
+        List<String> list = new ArrayList<>();
+        for(int i = 0;i<words.length;i++){
+            String s1 = i>0?words[i-1]:"";
+            String s2 = words[i];
+            char[] ch1 = s1.toCharArray();
+            char[] ch2 = s2.toCharArray();
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+            s1=Arrays.toString(ch1);
+            s2=Arrays.toString(ch2);
+            if(!s1.equals(s2)){
+                list.add(words[i]);
             }
         }
-        
-        List<String> res = new ArrayList<>();
-        
-        while(stk.size()>0){
-            res.add(stk.pop());
-        }
-        Collections.reverse(res);
-        return res;
+        return list;
     }
     public boolean isAnagram(String a, String b){
         int[] map = new int[26];
