5d7630aad12ffec87ff5fde5259d965c7b5bf099
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-19 13:38:37 +0530

    Time: 17 ms (25.00%), Space: 55.5 MB (100.00%) - LeetHub

--- 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
@@ -5,12 +5,7 @@ class Solution {
         int n = nums.length;
         int ans = Integer.MAX_VALUE;
         
-        ans = Math.min(ans , nums[n-1]-nums[0]);
-        ans = Math.min(ans , nums[n-1]-nums[2]);
-        ans = Math.min(ans , nums[n-3]-nums[0]);
-        ans = Math.min(ans , nums[n-2]-nums[1]);
-        
-        
+        ans = Math.min(Math.min(nums[n-1] - nums[2], nums[n-3] - nums[0]), nums[n-2] - nums[1]);
         return ans;
     }
 }
