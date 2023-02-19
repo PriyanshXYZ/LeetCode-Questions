4dea30bceb841b36d645b802173aa4cb623fb486
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-19 13:39:49 +0530

    Time: 16 ms (100.00%), Space: 55.7 MB (50.00%) - LeetHub

--- 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
@@ -1,6 +1,5 @@
 class Solution {
     public int minimizeSum(int[] nums) {
-        if(nums.length==3)return 0;
         Arrays.sort(nums);
         int n = nums.length;
         return Math.min(Math.min(nums[n-1] - nums[2], nums[n-3] - nums[0]), nums[n-2] - nums[1]);
