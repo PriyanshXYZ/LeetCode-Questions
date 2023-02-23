4c921381f34762e4d8e03a64dbd94a6ae902df35
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-23 22:52:37 +0530

    Time: 4 ms (57.52%), Space: 41.9 MB (62.00%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,4 +1,26 @@
 class Solution {
+    //more understandble solution
+    public int singleNumber(int[] nums) {
+        int[] bits = new int[32];
+		for(int val : nums){
+			int i = 0;
+			while(i < 32){
+				if((val & 1)>0){
+					bits[i]++;
+				}
+				i++;
+				val=val>>1;//left shifting the val
+			}
+		}
+
+		int ans = 0;
+		for(int i=0;i<32;i++){
+			if(bits[i]%3 == 1)ans = ans | (1<<i);// making a mask for ith bit  set if it contributes 3n+1 times
+		}
+		return ans;
+    }
+}
+class Solution1 {
     public int singleNumber(int[] nums) {
         int threeN = -1, threeNp1 = 0, threeNp2 = 0;
         for(int val : nums){
@@ -7,7 +29,7 @@ class Solution {
             int commonThreeNp2 = val & threeNp2;
             
             threeN = threeN & (~commonThreeN);
-            threeN = threeN | commonThreeNp2;
+            threeN = threeN | commonThreeNp2; // ans of 3n + 2 will contribute to 3n+3 as we have 
             
             threeNp1 = threeNp1 & (~commonThreeNp1);
             threeNp1 = threeNp1 | commonThreeN;
@@ -17,4 +39,5 @@ class Solution {
         }
         return threeNp1;
     }
+    
 }
