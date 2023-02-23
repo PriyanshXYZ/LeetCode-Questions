eb5283b7429fe7c070cba57377ca394e0922ba17
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-23 22:16:23 +0530

    Time: 100 ms (87.10%), Space: 59.8 MB (88.04%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,42 @@
+class Solution {
+    class Pair implements Comparable<Pair>{
+        int profit;
+        int capital;
+        
+        Pair(int profit, int capital){
+            this.profit = profit;
+            this.capital = capital;
+        }
+        
+        public int compareTo(Pair o){
+            return this.capital-o.capital;
+        }
+        
+        public String toString(){
+            return this.capital+" "+this.profit;
+        }
+    }
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        Pair[] projects = new Pair[profits.length];
+        
+        for(int i=0;i<projects.length;i++){
+            Pair p = new Pair(profits[i],capital[i]);
+            projects[i] = p;
+        }
+        int n = projects.length;
+        Arrays.sort(projects);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());//here pair wont work as we want to get maxprofit
+        int ptr = 0;
+        for(int i = 0; i < k; i++){
+            while(ptr < n && projects[ptr].capital<=w){
+                pq.add(projects[ptr].profit);
+                ptr++;
+            }
+            if(pq.size()==0)break;
+            
+            w += pq.remove();
+        }
+        return w;
+    }
+}
