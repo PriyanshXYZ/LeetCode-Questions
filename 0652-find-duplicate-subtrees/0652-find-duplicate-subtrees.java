10dabff77b7f66b30cb1cfc6ed8cad3ebbb64392
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-02-28 21:49:11 +0530

    Time: 2 ms (100.00%), Space: 42.7 MB (98.36%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -14,6 +14,82 @@
  * }
  */
 class Solution {
+    private Map<MyTreeNode, Integer> countMap = new HashMap<>();
+    private List<TreeNode> duplicates = new LinkedList<>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        dfs(root);
+        return duplicates;
+    }
+
+    private MyTreeNode dfs(TreeNode node) {
+        if(node == null) {
+            return null;
+        }
+
+        MyTreeNode left = dfs(node.left);
+        MyTreeNode right = dfs(node.right);
+
+        int prime = 31;
+        int hash = 1;
+        hash = hash * prime + node.val;
+        if(left != null) {
+            hash = hash * prime + left.hash;
+        }
+
+        if(right != null) {
+            hash = hash * prime + right.hash;
+        }
+
+        MyTreeNode myNode = new MyTreeNode(hash, node.val, left, right);
+        int count = countMap.getOrDefault(myNode, 0) + 1;
+        countMap.put(myNode, count);
+        if(count == 2) {
+            duplicates.add(node);
+        }
+
+        return myNode;
+    }
+
+    private class MyTreeNode {
+        private int hash;
+        private int val;
+        private MyTreeNode left;
+        private MyTreeNode right;
+
+        public MyTreeNode(int h, int v, MyTreeNode l, MyTreeNode r) {
+            this.hash = h;
+            this.val = v;
+            this.left = l;
+            this.right = r;
+        }
+
+        @Override
+        public int hashCode() {
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            MyTreeNode node = (MyTreeNode) o;
+            return this.hash == node.hash && this.val == node.val && equals(node.left, this.left) && equals(node.right, this.right);
+        }
+
+        private boolean equals(MyTreeNode a, MyTreeNode b) {
+            if(a == null && b == null) {
+                return true;
+            }
+
+            if(a == null || b == null) {
+                return false;
+            }
+
+            return a.equals(b);
+        }
+    }
+}
+
+class Solution1 {
     List<TreeNode> ans;
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
         HashMap<String, Integer> map = new HashMap<>();
@@ -30,13 +106,13 @@ class Solution {
         String rightStr = dfs(root.right, map);
         
         String subTreeStr ="(" + leftStr +")"+ root.val + "(" + rightStr+")";
-        if(map.containsKey(subTreeStr)){
-            
-            if(map.get(subTreeStr)==1){
-                ans.add(root);
-            }
+        
+        if(map.getOrDefault(subTreeStr,0)==1){
+            ans.add(root);
         }
+        
         map.put(subTreeStr,map.getOrDefault(subTreeStr,0)+1);
         return subTreeStr;
     }
+    
 }
