438440c8e27589143e5919d5cfbca510bebc0bd8
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-03-08 16:21:49 +0530

    Time: 75 ms (7.64%), Space: 61.1 MB (78.99%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.java
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.java
@@ -1,43 +1,38 @@
 class Solution {
     public int maxNumOfMarkedIndices(int[] nums) {
+        int cnt = 0, n = nums.length;
         
-        int res = 0;
         Arrays.sort(nums);
-
-        // visited array to mark the indices we visited , so not to take the 
-        // same indices again
-        boolean[] visited = new boolean[nums.length];
-        for(int i = 0;i<nums.length/2;i++)
-        {
-            int target = 2 * nums[i];
-            if(binarySearch(nums, target , i/2 , nums.length-1, visited))
-                res+=2;
+        boolean[] vis = new boolean[n];
+        for(int i=0;i<n/2;i++){
+            if(bs(i/2,n-1,2*nums[i], nums, vis)){
+                cnt+=2;
+            }
         }
-
-        return res;
+        return cnt;
     }
-  
-    boolean binarySearch(int [] nums , int target , int start , int end, boolean [] visited){
-
+    public boolean bs(int lo,int hi, int tar, int[] nums, boolean[] vis){
         boolean found = false;
         int index = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
 
-            if (target <= nums[mid] && !visited[mid]) {
+            if (tar <= nums[mid] && !vis[mid]) {
+                
                 found = true;
-                index = mid;
-                end = mid - 1;
+                index = mid;    
+                
+                hi = mid - 1;
 
             } else {
-                start = mid + 1;
+                lo = mid + 1;
             }
 
         }
 
 
         if (found)
-            visited[index] = true;
+            vis[index] = true;
 
         return found;
     }
