e1313439cf850ddf7a5d85f94e8200c2dfcedafb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-03-11 12:02:48 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (75.75%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -25,6 +25,29 @@
  */
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
+        if(head==null)return null;
+        if(head.next==null)return new TreeNode(head.val);
+        
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        prev.next =null;
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
+class Solution1 {
+    //extra space...
+    public TreeNode sortedListToBST(ListNode head) {
         //even numbers me balance krna is not possible
         //odd me humhesha hi balance rehega
         if(head==null)return null;
