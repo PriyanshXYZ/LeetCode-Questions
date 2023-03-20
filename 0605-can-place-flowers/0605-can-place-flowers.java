5d63ca6f2e570dd80f0b3afc58e1dec4d65a78ae
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-03-21 01:16:05 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (23.50%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -3,16 +3,16 @@ class Solution {
         //appraoch is to fill max possible number of 1's in subarray of only zero having boundary of 1 
         
         int count = 1;
-        int result = 0;
-        for(int i=0; i<flowerbed.length; i++) {
-            if(flowerbed[i] == 0) {
-                count++;
-            }else {
-                result += (count-1)/2;
+        int max = 0;
+        for(int i  = 0;i<flowerbed.length;i++){
+            if(flowerbed[i]==1){
+                max += (count - 1)/ 2;
                 count = 0;
+            }else{
+                count++;
             }
         }
-        if(count != 0) result += count/2;
-        return result>=n;
+        if(count!=0)max += count/2;
+        return max>=n;
     }
 }
