6555fb4ace2a26ed8e8097e91f345f673e094e4a
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-03-27 00:06:07 +0530

    Time: 23 ms (91.89%), Space: 60.8 MB (78.76%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,4 +1,56 @@
 class Solution {
+    public int longestCycle(int[] edges) {
+        int n = edges.length;
+        boolean[] visit = new boolean[n];
+        int[] indegree = new int[n];
+
+        // Count indegree of each node.
+        for (int edge : edges) {
+            if (edge != -1) {
+                indegree[edge]++;
+            }
+        }
+
+        // Kahn's algorithm starts.
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            visit[node] = true;
+            int neighbor = edges[node];
+            if (neighbor != -1) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
+            }
+        }
+        // Kahn's algorithm ends.
+
+        int answer = -1;
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                int neighbor = edges[i];
+                int count = 1;
+                visit[i] = true;
+                // Iterate in the cycle.
+                while (neighbor != i) {
+                    visit[neighbor] = true;
+                    count++;
+                    neighbor = edges[neighbor];
+                }
+                answer = Math.max(answer, count);
+            }
+        }
+        return answer;
+    }
+}
+class Solution1 {
     int cycleSize=-1;
     public void dfs(int src, int[] graph, int[] vis,boolean[] pathVis,int sum){
         sum += 1;
