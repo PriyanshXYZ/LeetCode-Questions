727b218acdee3303872574ef4b564c105a9f7c7f
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-04-23 12:01:27 +0530

    Time: 414 ms (9.20%), Space: 70.8 MB (5.75%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    static final int MOD = (int)(1e9+7);
+    Long[] dp = new Long[(int)1e5+1];
+    public int numberOfArrays(String s, int k) {
+        return (int)(recursion(0,s,k) % MOD);
+    }
+    public long recursion(int idx, String s, int k){
+        //base case
+        if(idx >= s.length())return 1;
+        if(s.charAt(idx)=='0')return 0;
+        
+        if(dp[idx] != null)return dp[idx];
+        
+        long ans = 0;
+        for(int i=idx+1;i<=s.length();i++){
+            long num = Long.parseLong(s.substring(idx,i));
+            if(num<=k){
+                ans = ans % MOD  + recursion(i,s,k) % MOD;
+            }else break;
+        }
+        return dp[idx] = ans % MOD;
+    }
+}
