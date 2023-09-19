9565a014d92f04ff1f273c612ee41e840d0187d6
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-09-19 22:47:26 +0530

    Time: 4 ms (87.14%), Space: 56.5 MB (46.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,4 +1,8 @@
 class Solution {
+    //approach 1 sort and then check if i+1 !=nums[i] return it
+    //approach 2 use hashset to get duplicate ..
+    //approach 3 using linkedlist cycle detection... i.e if there is duplicacy we can get to that point again...so there would be a cycle we just have to start val of cycle ..using flyod cycle detection algo..
+    
     public int findDuplicate(int[] nums) {
         int slow=nums[0];
         int fast=nums[0];
