bd2bbdd3bf13afd708b3099d20c417c4b54b3a29
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-10-21 17:15:43 +0530

    Time: 84 ms (25.20%), Space: 61 MB (5.51%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int constrainedSubsetSum(int[] nums, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            return b[0] - a[0];
+        });
+        
+        heap.add(new int[] {nums[0], 0});
+        int ans = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            while (i - heap.peek()[1] > k) {
+                heap.remove();
+            }
+            
+            int curr = Math.max(0, heap.peek()[0]) + nums[i];
+            ans = Math.max(ans, curr);
+            heap.add(new int[] {curr, i});
+        }
+        
+        return ans;
+    }
+}
+//     public int recursion(int idx, int k, int[] nums, int sum){
+//         if(idx==nums.length)return sum;
+        
+//         int take = recursion(idx + 1, k , nums, sum + nums[idx]);
+//         int notTake = recursion(idx + 1, k , nums, sum );
+        
+//         return Math.max(take, notTake);
+//     }
+// }
