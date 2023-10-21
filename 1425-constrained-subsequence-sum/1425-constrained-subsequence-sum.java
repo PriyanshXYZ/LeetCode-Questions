342b55696d1552cb472558aa6396b687d16d675b
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-10-21 17:37:17 +0530

    Time: 33 ms (94.49%), Space: 56.8 MB (49.61%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
@@ -1,22 +1,47 @@
 class Solution {
     public int constrainedSubsetSum(int[] nums, int k) {
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
-            return b[0] - a[0];
-        });
+//         PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+//             return b[0] - a[0];
+//         });
         
-        heap.add(new int[] {nums[0], 0});
-        int ans = nums[0];
+//         heap.add(new int[] {nums[0], 0});
+//         int ans = nums[0];
         
-        for (int i = 1; i < nums.length; i++) {
-            while (i - heap.peek()[1] > k) {
-                heap.remove();
+        
+//         for (int i = 1; i < nums.length; i++) {
+//             while (i - heap.peek()[1] > k) {
+//                 heap.remove();
+//             }
+            
+//             int curr = Math.max(0, heap.peek()[0]) + nums[i];
+//             ans = Math.max(ans, curr);
+//             heap.add(new int[] {curr, i});
+//         }
+        
+//         return ans;
+        Deque<Integer> q = new ArrayDeque<>();
+        int[] dp = new int[nums.length];
+        
+        for(int i = 0;i < nums.length; i++){
+            if(!q.isEmpty() && i - q.peek() > k){
+                q.poll();
             }
             
-            int curr = Math.max(0, heap.peek()[0]) + nums[i];
-            ans = Math.max(ans, curr);
-            heap.add(new int[] {curr, i});
+            dp[i] = (!q.isEmpty() ? dp[q.peek()]:0)+nums[i];
+            
+            while(!q.isEmpty()&&  dp[q.peekLast()] < dp[i]){
+                q.pollLast();
+            }
+            
+            if(dp[i] > 0){
+                q.add(i);
+            }
         }
         
+        int ans = Integer.MIN_VALUE;
+        for(int num : dp){
+            ans = Math.max(ans, num);
+        }
         return ans;
     }
 }
