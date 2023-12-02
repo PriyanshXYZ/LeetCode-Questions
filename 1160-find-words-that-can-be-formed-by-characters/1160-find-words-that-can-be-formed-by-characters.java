004df37d142df6a7f667eebc17c5ea6c60b5a4d0
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2023-12-03 01:47:39 +0530

    Time: 10 ms (53.69%), Space: 44 MB (69.59%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        int ans = 0;
+        for(String word : words){
+            freq1 = new int[26];
+            freq2 = new int[26];
+            for(char ch : word.toCharArray()){
+                freq1[ch-'a']++;
+            }
+            for(char ch : chars.toCharArray()){
+                freq2[ch-'a']++;
+            }
+            boolean flag = true;
+            int cnt = 0;
+            for(int i=0;i<26;i++){
+                if(freq2[i]>=freq1[i]){
+                    cnt += freq1[i];
+                }else{
+                    cnt = 0;
+                    break;
+                }
+            }
+            ans += cnt;
+        }
+        return ans;
+        // for(char ch : chars.toCharArray()){
+        //     freq2[ch-'a']++;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq2[i]>=freq1[i]){
+        //         ans += freq2[i];
+        //     }
+        // }
+        // return ans;
+    }
+}
