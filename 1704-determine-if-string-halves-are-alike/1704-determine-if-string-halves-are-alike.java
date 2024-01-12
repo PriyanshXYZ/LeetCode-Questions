308cd8b3fd7f512667635f2655237d7834b151b2
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-01-12 23:29:20 +0530

    Time: 3 ms (88.24%), Space: 41.3 MB (46.98%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        s = s.toLowerCase();
+        // s = s.toLowerCase();
         
         int cnt = 0;
         for(int i=0, j=s.length()/2;i<s.length()/2;i++,j++){
             char ch = s.charAt(i);
             char chh = s.charAt(j);
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
                 cnt++;
             }
-            if(chh=='a' || chh=='e' || chh=='i' || chh=='o' || chh=='u'){
+            if(chh=='a' || chh=='e' || chh=='i' || chh=='o' || chh=='u' || chh=='A' || chh=='E' || chh=='I' || chh=='O' || chh=='U'){
                 cnt--;
             }
         }
