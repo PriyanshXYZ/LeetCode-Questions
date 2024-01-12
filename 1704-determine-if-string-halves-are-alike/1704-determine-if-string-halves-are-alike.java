65ad1dad92755454c4eb4fa724ddede5a7fb751d
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-01-12 23:25:03 +0530

    Time: 6 ms (42.07%), Space: 41.6 MB (34.03%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int vowleft=0,vowright=0;
-        int i=0,j=s.length()/2;
-        while(i<s.length()/2){
-            char ch1=s.charAt(i);
-            char ch2=s.charAt(j);
-            if(ch1=='a' || ch1=='e' || ch1=='i' ||ch1=='o' || ch1=='u' || ch1=='A' || ch1=='E' || ch1=='I' ||ch1=='O' || ch1=='U')vowleft++;
-            if(ch2=='a' || ch2=='e' || ch2=='i' ||ch2=='o' || ch2=='u' || ch2=='A' || ch2=='E' || ch2=='I' ||ch2=='O' || ch2=='U')vowright++;
-            i++;
-            j++;
+        s = s.toLowerCase();
+        
+        int cnt = 0;
+        for(int i=0;i<s.length()/2;i++){
+            char ch = s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                cnt++;
+            }
         }
-        return vowleft==vowright;
+        for(int i=s.length()/2;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                cnt--;
+            }
+        }
+        return cnt==0;
     }
-    
 }
