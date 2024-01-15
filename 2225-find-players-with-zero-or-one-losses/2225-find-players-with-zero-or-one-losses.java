aa61f3627d9f9e518ce164f868fca251dc7ab259
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-01-16 00:02:15 +0530

    Time: 97 ms (33.49%), Space: 111.3 MB (7.26%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,32 +1,25 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        HashMap<Integer,int[]> map=new HashMap();
-        for(int[] match:matches){
-            int winner=match[0];
-            int loser=match[1];
-            if(!map.containsKey(winner)){
-                map.put(winner,new int[2]);
-            }
-            if(!map.containsKey(loser)){
-                map.put(loser,new int[2]);
-            }
-            map.get(winner)[0]++;
-            map.get(loser)[1]++;
-        }
+        List<List<Integer>> res = new ArrayList<>();
+        Map<Integer,Integer> lose = new HashMap<>();
+        Map<Integer,Integer> win = new HashMap<>();
         
-        List<List<Integer>> ans=new ArrayList();
-        ans.add(new ArrayList());
-        ans.add(new ArrayList());
-        for(int val:map.keySet()){
-            if(map.get(val)[1]==0){
-                ans.get(0).add(val);
-            }
-            if(map.get(val)[1]==1){
-                ans.get(1).add(val);
-            }
+        for(int[] match : matches){
+            win.put(match[0],win.getOrDefault(match[0],0)+1);
+            lose.put(match[1],lose.getOrDefault(match[1],0)+1);
         }
-        Collections.sort(ans.get(0));
-        Collections.sort(ans.get(1));
-        return ans;
+        // System.out.println(win);
+        // System.out.println(lose);
+        res.add(new ArrayList<>());
+        res.add(new ArrayList<>());
+        for(int key : win.keySet()){
+            if(!lose.containsKey(key))res.get(0).add(key);
+        }
+        for(int key : lose.keySet()){
+            if(lose.get(key) == 1)res.get(1).add(key);
+        }
+        Collections.sort(res.get(0));
+        Collections.sort(res.get(1));
+        return res;
     }
 }
