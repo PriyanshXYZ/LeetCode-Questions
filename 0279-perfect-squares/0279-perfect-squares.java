8a23e34134e02b7c3b17dd4dfebd9a5209c1aad4
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-02-08 23:49:19 +0530

    Time: 62 ms (37.10%), Space: 42.5 MB (79.98%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -4,14 +4,18 @@ class Solution {
         Arrays.fill(dp,-1);
         return recursion(n,dp);
     }
+    
     public int recursion(int n,int[] dp){
-        if(n==0)return 0;
+        if(n==0){
+            return 0;
+        }
         if(dp[n]!=-1)return dp[n];
         
         int ans=Integer.MAX_VALUE;
         for(int i=(int)Math.pow(n,0.5);i>0;i--){
             
             ans=Math.min(ans,1+recursion(n-i*i,dp));
+            
         }
         
         return dp[n]=ans;
