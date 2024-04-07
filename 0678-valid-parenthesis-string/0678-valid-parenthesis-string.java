3a9f0b9359e371f05c8e0d80eda62188fa3d379b
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-04-07 13:13:13 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (30.05%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,52 +1,28 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Integer> open = new Stack<>();
-        Stack<Integer> star = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(') {open.push(i);}
-            else if(s.charAt(i) == '*') {star.push(i);}
-            else {
-                if(!open.isEmpty()) {open.pop();} // found matched '('with')'
-                else if (!star.isEmpty()) {star.pop();} // match '*' with ')'
-                else return false; // cannot find match
+        Stack<Integer> stk = new Stack<>();
+        Stack<Integer> dot = new Stack<>();
+        int idx = 0;
+        for(char ch : s.toCharArray()){
+            if(ch=='('){
+                stk.push(idx);
+            }else if(ch=='*'){
+                dot.push(idx);
+            }else{
+                if(!stk.isEmpty())stk.pop();
+                else if(!dot.isEmpty())dot.pop();
+                else return false;
             }
+            idx++;
         }
-
-        // checking leftover on 2 stack
-        while(!open.isEmpty()) { // we can have '*' leftover, but 'open' must run out 
-            if(star.isEmpty()) {return false;} // '(' leftover
-            else if (open.peek()<star.peek()){open.pop(); star.pop();} // '*'index > '(' index, matched
-            else {return false;} // open>top, corner case
+        
+        while(!stk.isEmpty()){
+            if(dot.isEmpty())return false;
+            if(dot.peek()>stk.peek()){
+                stk.pop();
+                dot.pop();
+            }else return false;
         }
         return true;
     }
-}
-class Solution1 {
-    public boolean checkValidString(String s) {
-        Stack<Character> bkt = new Stack<>();
-        Stack<Character> str = new Stack<>();
-		for(char ch : s.toCharArray()){
-			if(ch=='('){
-                bkt.push(ch);
-            }else if(ch=='*'){
-                str.push(ch);
-            }else{
-                if(!bkt.isEmpty())bkt.pop();//we found opening for our closing
-                else if(!str.isEmpty())str.pop();
-                else return false;
-            }
-		}
-        
-        while(!bkt.isEmpty()){
-            if(bkt.peek()<str.peek()){
-                bkt.pop();
-                str.pop();
-            }else{
-                return false;
-            }
-        }
-		return true;
-    }
-    
 }
