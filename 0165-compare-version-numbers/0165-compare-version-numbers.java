a18fa48e483781e05e4ed36c46def8e7ed8651fa
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-05-04 00:28:41 +0530

    Time: 1 ms (78.87%), Space: 41.2 MB (70.46%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -9,7 +9,7 @@ class Solution {
         while(idx1<str1.length || idx2<str2.length){
             Integer res1 = Integer.parseInt(idx1<str1.length?str1[idx1]:"0");
             Integer res2 = Integer.parseInt (idx2<str2.length?str2[idx2]:"0");
-            System.out.println(res1+" "+res2);
+            
             int val = res1.compareTo(res2);
             if(val!=0)return val;
             idx1++;
