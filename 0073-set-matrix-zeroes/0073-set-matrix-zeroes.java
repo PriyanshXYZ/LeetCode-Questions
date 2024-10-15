be6f432a3f2b71c463135b32ea4fc8cd9b728f21
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2024-10-16 00:21:03 +0530

    Time: 1 ms (78.48%), Space: 45.1 MB (96.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,80 +1,42 @@
 class Solution {
-    public void setZeroes(int[][] a) {
-        int n=a.length;
-        int m=a[0].length;
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
         
-        boolean col0=false;
+        boolean isZeroCol = false;
+       
         
         for(int i=0;i<n;i++){
-            if(a[i][0]==0){
-                col0=true;
+            if(matrix[i][0] == 0){
+                isZeroCol = true;
             }
             for(int j=1;j<m;j++){
-                if(a[i][j]==0){
-                    a[0][j]=0;
-                    a[i][0]=0;
-                }
-            }
-        }
-        
-        //iterating thorugh cols
-        for(int j=1;j<m;j++){
-            for(int i=1;i<n;i++){
-                if(a[0][j]==0 || a[i][0]==0){
-                    a[i][j]=0;
-                }
-            }
-        }
-        
-        if(a[0][0]==0){
-            for(int j=0;j<m;j++){
-                a[0][j]=0;
-            }    
-        }
-        if(col0){
-            for(int i=0;i<n;i++){
-                a[i][0]=0;
-            }
-        }
-        
-    }
-}
-class Solution1 {
-    class Pair{
-        int x;
-        int y;
-        
-        Pair(int x,int y){
-            this.x=x;
-            this.y=y;
-        }
-    }
-    //time complexity O(n*m)  space compelxty O(zeros)
-    public void setZeroes(int[][] matrix) {
-        List<Pair> zeros=new ArrayList();
-        int n=matrix.length;
-        int m=matrix[0].length;
-        //getting zeros
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    Pair p=new Pair(i,j);
-                    zeros.add(p);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+            
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[j][0] == 0 || matrix[0][i]==0){
+                    matrix[j][i]=0;
                 }
             }
         }
-        if(zeros.size()==m*n)return;
-        for(Pair p:zeros){
-            int r=p.x;
-            int c=p.y;
-            
-            for(int i=0;i<n;i++){
-                matrix[i][c]=0;
+        
+        if(matrix[0][0] == 0){
+            for(int i=0;i<m;i++){
+                matrix[0][i] = 0;
             }
-            for(int j=0;j<m;j++){
-                matrix[r][j]=0;
+        }
+        
+        if(isZeroCol){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
     }
-    
 }
