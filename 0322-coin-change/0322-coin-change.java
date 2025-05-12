cb41fca73bc3495fa14aaf07180d5eb151e00e00
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-05-12 22:47:21 +0530

    Time: 17 ms (47.94%), Space: 44.3 MB (80.4%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    /*
+     1 2 5  14
+     5 5 2 2
+          0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+        1 M 1 2 3 4 5 6 7 8 9 10 11 12 13 14 
+        2 M M 1 2 2 3 3 4 4 5  5  6  6  7  7
+        5 M 1 1 2 2 1 2 2 3 3  2  2  2  3  3
+    */
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        if(n==1)return amount%coins[0]>0?-1:amount/coins[0];
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int amt=0;amt<=amount;amt++){
+            for(int i=0;i<n;i++){
+                int val = amt>=coins[i]?dp[amt - coins[i]]:Integer.MAX_VALUE;
+                if(val!=Integer.MAX_VALUE){
+                    dp[amt] = Math.min(dp[amt], val + 1);
+                }
+            }
+        }
+
+        return dp[amount]!=Integer.MAX_VALUE?dp[amount]:-1;
+    }
+}
