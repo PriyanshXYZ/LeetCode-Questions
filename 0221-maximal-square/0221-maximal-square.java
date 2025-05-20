4816ff18f112a06fe795905004ce1b032c550af6
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-05-20 20:15:55 +0530

    Time: 8 ms (71.01%), Space: 60.5 MB (45.66%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -12,23 +12,43 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        Integer[][] dp = new Integer[n+1][m+1];
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m ; j++){
-                if(matrix[i][j] == '1'){
-                    ans = Math.max(memoize(i, j, n, m, matrix, dp), ans);
-                }
+        int[][] dp = new int[n+1][m+1];
+        int maxLen = 0;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0] == '1'){
+                dp[i][0] = 1;
+                maxLen = 1;
             }
         }
 
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < m ; j++){
-        //         System.out.print(matrix[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        return ans*ans;
+        for(int j=0;j<m;j++){
+            if(matrix[0][j] == '1'){
+                dp[0][j] = 1;
+                maxLen = 1;
+            }
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m ; j++){
+                int ans = 0;
+                if(matrix[i][j] == '1'){
+                    ans = 1;
+                    int ansLeft = dp[i][j - 1];
+                    int ansTop = dp[i - 1][j];
+                    int ansDiagonal = dp[i - 1][j - 1];
+
+                    int minEdgeLength = Math.min(ansLeft, ansTop);
+                    minEdgeLength = Math.min(minEdgeLength, ansDiagonal);
+
+                    ans += minEdgeLength;
+                }
+
+                dp[i][j] = ans;
+                maxLen = Math.max(ans, maxLen);
+            }
+        }
+
+        return maxLen * maxLen;
     }
 
     public int memoize(int currRow, int currCol, int n , int m, char[][] matrix, Integer[][] dp) {
