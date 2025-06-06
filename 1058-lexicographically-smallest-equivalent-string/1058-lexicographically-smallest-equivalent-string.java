c881c8e8bcab28b5f2cb7ab31f64f0955856d50a
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-06-06 17:46:24 +0530

    Time: 2 ms (96.73%), Space: 41.9 MB (77.64%) - LeetHub

---
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
@@ -0,0 +1,50 @@
+class Solution {
+    class UF{
+        int[] parent;
+        int[] rank;
+        
+        UF(){
+            parent = new int[26];
+            rank = new int[26];
+            for(int i =0;i<26;i++){
+                parent[i]=i;
+            }
+        }
+        int find(int x){
+            if(parent[x]!=x){
+                return parent[x]=find(parent[x]);
+            }
+            return x;
+        }
+        void union(int x,int y){
+            int xlead = find(x);
+            int ylead = find(y);
+            
+            
+            if(xlead!=ylead){
+                //rank given based on lexicographically smaller occurance
+                if(xlead<ylead){
+                    parent[ylead]=xlead;
+                    rank[xlead]++;
+                }else{
+                    parent[xlead]=ylead;
+                    rank[ylead]++;
+                }
+            }
+        }
+    }
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int n = s1.length();
+        UF uf = new UF();
+        for(int i = 0;i<n;i++){
+            uf.union(s1.charAt(i)-'a',s2.charAt(i)-'a');
+        }
+        
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i<baseStr.length();i++){
+            int ch = baseStr.charAt(i)-'a';
+            res.append((char)(uf.find(ch)+'a'));
+        }
+        return res.toString();
+    }
+}
