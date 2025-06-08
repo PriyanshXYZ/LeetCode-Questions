98e4e32dfdd1858e1c6f069c80eb6392decbad19
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-06-08 12:06:22 +0530

    Time: 103 ms (86.78%), Space: 48.1 MB (62.07%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
@@ -1,6 +1,38 @@
 class Solution {
     public String clearStars(String s) {
         int n = s.length();
+        Stack<Integer>[] occuranceStack = new Stack[26];
+        for(int i = 0; i < 26; i++) {
+            occuranceStack[i] = new Stack();
+        }
+        char[] str = s.toCharArray();
+        for(int i = 0; i < n; i++) {
+            char ch = str[i];
+            if(ch!='*') {
+                occuranceStack[ch-'a'].add(i);
+            }else{
+                for(int j = 0; j < 26; j++) {
+                    if(!occuranceStack[j].isEmpty()){
+                        int idx = occuranceStack[j].pop();
+                        str[idx]='*';
+                        break;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            char ch = str[i];
+            if(ch!='*') sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
+class Solution1 {
+    //nlogn
+    public String clearStars(String s) {
+        int n = s.length();
         PriorityQueue<Integer> idxMinHeap = new PriorityQueue<>((a,b) -> s.charAt(a)==s.charAt(b)?(b-a):(s.charAt(a)-s.charAt(b)));
         Set<Integer>  toBeRemovedIdx = new HashSet();
         for(int i =0 ; i < n; i++) {
