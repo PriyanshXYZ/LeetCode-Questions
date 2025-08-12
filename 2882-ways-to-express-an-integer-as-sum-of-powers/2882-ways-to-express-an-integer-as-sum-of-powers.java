0fb4ebff3224dffa3ee2540ae80f05d9bd789ffb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-08-13 00:42:50 +0530

    Time: 294 ms (20.53%), Space: 45.2 MB (28.95%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,26 +1,27 @@
-class Solution {
-    public int numberOfWays(int n, int x) {
-        final int MOD = 1_000_000_007;
-        
-        // dp[i] stores the number of ways to form sum i
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // Base case: one way to form a sum of 0
-
-        // Find all unique positive integers whose x-th power is <= n
-        for (int i = 1; ; i++) {
-            long power = (long) Math.pow(i, x);
+    class Solution {
+        Integer[][] dp = new Integer[302][302];
+        int mod = (int)(1e9 + 7);
+        public int numberOfWays(int n, int x) {
+            int maxBase = (int) Math.pow(n, 1.0 / x);
             
-            if (power > n) {
-                break;
-            }
-            
-            // Iterate backwards to avoid using the same power multiple times
-            // in a single combination.
-            for (int j = n; j >= power; j--) {
-                dp[j] = (dp[j] + dp[j - (int) power]) % MOD;
-            }
+            return memo(n, x, 1);
         }
+
+        private int memo(int n, int x, int currNum) {
+            //base case
+            if(n<0)return 0;
+            if(n==0)return dp[n][currNum] = 1;
+            int pow = (int)Math.pow(currNum,x);
+            if(pow > n)return 0;
+
+            if(dp[n][currNum] != null) return dp[n][currNum];
+            int ans = 0;
+            
+           
+            int possible = memo(n - pow, x, currNum + 1);
+            int skip = memo(n, x, currNum + 1);
+            ans += (skip % mod + possible % mod) % mod ;
         
-        return dp[n];
+            return dp[n][currNum] = ans;
+        }
     }
-}
