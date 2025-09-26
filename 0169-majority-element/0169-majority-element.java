b2d2138b3c70efed8e851ede16ee69a5a15c5345
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-09-26 23:41:09 +0530

    Time: 21 ms (9.44%), Space: 49.9 MB (74.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,53 @@
 class Solution {
-   public int majorityElement(int[] a) {
-        int majEle=a[0];
-        int freq=1;
-        for(int i=1;i<a.length;i++){
-            if(a[i]==majEle){
-                freq++;
-            }else{
-                if(freq>0){
-                    freq--;
-                }else{
-                    majEle=a[i];
-                    freq=1;
+    public int majorityElement(int[] a) {
+        return majorityElements(a, 2).get(0);
+    }
+
+    public static List<Integer> majorityElements(int[] nums, int k) {
+        if (k < 2) throw new IllegalArgumentException("k must be >= 2");
+
+        // Step 1: Find potential candidates (at most k-1)
+        Map<Integer, Integer> candidates = new HashMap<>();
+
+        for (int num : nums) {
+            if (candidates.containsKey(num)) {
+                candidates.put(num, candidates.get(num) + 1);
+            } else if (candidates.size() < k - 1) {
+                candidates.put(num, 1);
+            } else {
+                // Decrement all counts
+                List<Integer> toRemove = new ArrayList<>();
+                for (Map.Entry<Integer, Integer> entry : candidates.entrySet()) {
+                    int newCount = entry.getValue() - 1;
+                    if (newCount == 0) {
+                        toRemove.add(entry.getKey());
+                    } else {
+                        candidates.put(entry.getKey(), newCount);
+                    }
+                }
+                // Remove zero-count candidates
+                for (int key : toRemove) {
+                    candidates.remove(key);
                 }
             }
         }
-        return majEle;
+
+        // Step 2: Verify actual counts
+        Map<Integer, Integer> actualCounts = new HashMap<>();
+        for (int num : nums) {
+            if (candidates.containsKey(num)) {
+                actualCounts.put(num, actualCounts.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : actualCounts.entrySet()) {
+            if (entry.getValue() > nums.length / k) {
+                result.add(entry.getKey());
+            }
+        }
+
+        return result;
     }
 }
 //with space
