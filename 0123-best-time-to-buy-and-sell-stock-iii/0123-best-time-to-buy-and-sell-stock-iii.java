b5933d11e9668593d30497d32a899a21cc199250
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2025-11-02 22:15:17 +0530

    Time: 1 ms (99.83%), Space: 75.9 MB (22.74%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,11 +1,11 @@
 class Solution {
     private static final int BUY = 0;
     private static final int SELL = 1;
-    public int maxProfit(int[] prices) {
+    public int tabulation(int[] prices) {
         int n = prices.length;
         if (n == 0) return 0;
         
-        int[][][] dp = new int[n][2][3]; // [day][buy or sell][transactions completed]
+        int[][][] dp = new int[n][2]  [3]; // [day][buy or sell][transactions completed]
 
         // Initialization for day 0
         for (int t = 0; t <= 2; t++) {
@@ -22,6 +22,20 @@ class Solution {
         }
         
         return dp[n-1][SELL][2]; // max profit with 2 transactions and can buy (no stock in hand)
+    }
+    public int maxProfit(int[] prices) {
+        
+        int buy1 = Integer.MIN_VALUE, sell1 = 0;
+        int buy2 = Integer.MIN_VALUE, sell2 = 0;
+
+        for (int price : prices) {
+            buy1  = Math.max(buy1, -price);          // first buy
+            sell1 = Math.max(sell1, buy1 + price);   // first sell
+            buy2  = Math.max(buy2, sell1 - price);   // second buy
+            sell2 = Math.max(sell2, buy2 + price);   // second sell
+        }
+
+        return sell2;
     }
 
     private static int getMaxProfit(int idx, int transaction, int buyOrSell, int[] prices , Integer[][][] dp) {
