85849042263a7e1b34f09f0ef42224cfe8d7a354
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2026-06-18 00:29:40 +0530

    Time: 0 ms (100%), Space: 43.2 MB (31.8%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -17,7 +17,37 @@ class Solution {
     List<Integer> path;
     public List<Integer> inorderTraversal(TreeNode root) {
         path = new ArrayList<>();
-        dfs(root);
+        // dfs(root);
+        //using morris traversal
+        TreeNode curr = root;
+
+        while(curr!=null) {
+            if(curr.left==null){
+                //leaf node case
+                path.add(curr.val);
+                curr = curr.right;
+            }else {
+
+                //find the right most in left subtree
+                TreeNode inOrderPredecessor = curr.left; 
+                while(true) {
+                    if(inOrderPredecessor.right == null) break;
+                    if(inOrderPredecessor.right == curr) break;
+
+                    inOrderPredecessor = inOrderPredecessor.right;
+                }
+
+                //left subtree is not explored
+                if(inOrderPredecessor.right == null) {
+                    inOrderPredecessor.right = curr; //create thread
+                    curr = curr.left;
+                }else if(inOrderPredecessor.right == curr){ //right subtree is 
+                    path.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+
         return path;
     }
 
