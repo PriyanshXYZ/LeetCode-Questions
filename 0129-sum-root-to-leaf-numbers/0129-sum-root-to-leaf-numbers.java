e1fc4ad3902b06afe3592d2976eca7b1c97bedcb
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2026-06-21 02:12:59 +0530

    Time: 4 ms (4.57%), Space: 43.3 MB (12.46%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,62 +14,23 @@
  * }
  */
 class Solution {
-    public int sumNumbers(TreeNode root) {
-        return dfs(root,0);
-    }
-    //sol 1 using return type
-    private int dfs(TreeNode root,int  currSum){
-        if(root==null)return 0;
-        currSum=currSum*10+root.val;
-        if(root.left==null && root.right==null){
-          return currSum;
-        }
-        
-        int leftSum=dfs(root.left,currSum);
-        int rightSum=dfs(root.right,currSum);
-        return leftSum+rightSum;
-    } 
-}
-//sol2 using static variable
-class Solution2 {
-    int sum=0;
+    int ans = 0;
     public int sumNumbers(TreeNode root) {
         dfs(root,0);
-        return sum;
+        return ans;
     }
-    
-    private void dfs(TreeNode root,int  currSum){
-        if(root==null)return;
-        currSum=currSum*10+root.val;
-        if(root.left==null && root.right==null){
-           sum+=currSum;
+
+    private void dfs(TreeNode node, int path) {
+        if(node == null)return;
+        path*=10;
+        path+=node.val;
+        if(node.left == null && node.right == null) {
+            System.out.println(path);
+            ans += path;
+            return;
         }
-        
-        dfs(root.left,currSum);
-        dfs(root.right,currSum);
-        
-    } 
-}
-//sol3 using list to store String value =>brute force
-class Solution3 {
-    List<String> list=new ArrayList();
-    public int sumNumbers(TreeNode root) {
-        dfs(root,"");
-        int sum=0;
-        for(int i=0;i<list.size();i++){
-            sum+=Integer.parseInt(list.get(i));
-        }
-        return sum;
+
+        dfs(node.left, path);
+        dfs(node.right, path);
     }
-    
-    private void dfs(TreeNode root,String op){
-        if(root==null)return;
-        op+=root.val;
-        if(root.left==null && root.right==null){
-            list.add(op);
-        }
-        
-        dfs(root.left,op);
-        dfs(root.right,op);
-    } 
 }
