08bf18ee45ec0ef6d1636c984f738a3254f03094
Priyansh Ragit <90685216+PriyanshXYZ@users.noreply.github.com>
2026-08-06 11:20:35 +0530

    Time: 47 ms (31.53%), Space: 109 MB (73.15%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -1,43 +1,46 @@
 class Solution {
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        ArrayList<Integer>[] graph =new ArrayList[n];
-        
-        for(int i=0;i<n;i++){
-            graph[i]=new ArrayList();
+        //dfs 1 -> find the sum for root and get the array of size of subtree for every node
+        //dfs 2 -> just ans[parent] - subtreeSize[child] + n - subtreeSize[child]
+        List<List<Integer>> graph = new ArrayList();
+        for(int i = 0; i < n; i++) {
+            graph.add(new ArrayList());
+        }
+
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u); // remember to make tree bidirectional
         }
         
-        //creating a graph
-        for(int i=0;i<edges.length;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            
-            graph[u].add(v);
-            graph[v].add(u);
-        }
-        int[] nodes=new int[n];
-        int[] res=new int[n];
-        
-        helper1(graph,nodes,res,0,-1);
-        helper2(graph,nodes,res,0,-1);
-        return res;
+        int[] subtreeSize = new int[n];
+        int currSum = dfs1(0,-1, 0, graph, subtreeSize);
+
+        int[] dist = new int[n];
+        dfs2(0, -1, currSum, graph, dist, subtreeSize);
+
+        return dist;
     }
-    private void helper1(ArrayList<Integer>[] graph,int[] nodes,int[] res,int src,int parent){
-        
-        for(int nbr:graph[src]){
-            if(nbr!=parent){
-                helper1(graph,nodes,res,nbr,src);
-                nodes[src]+=nodes[nbr];//to add descendants count
-                res[src]+=nodes[nbr]+res[nbr];
-            }
+
+    private int dfs1(int node,int par, int currDist, List<List<Integer>> graph, int[] subtreeSize) {
+        subtreeSize[node] += 1;
+        int ans = currDist ;
+        for(int child : graph.get(node)){
+            if(child==par)continue;
+            ans += dfs1(child, node, currDist + 1, graph, subtreeSize);
+            subtreeSize[node] += subtreeSize[child];
         }
-        nodes[src]++;//to add self count
+        return ans;
     }
-    private void helper2(ArrayList<Integer>[] graph,int[] nodes,int[] res,int src,int parent){
-        for(int nbr:graph[src]){
-            if(nbr!=parent){
-                res[nbr]=res[src]+(nodes.length-nodes[nbr])-(nodes[nbr]); //making parent as a child as we go towards child , we move away from parent nodes
-                helper2(graph,nodes,res,nbr,src);
-            }
+
+    private void dfs2(int node,int par, int currSum, List<List<Integer>> graph, int[] dist, int[] subtreeSize) {
+        dist[node] = currSum;
+
+        for(int child : graph.get(node)) {
+            if(child==par)continue;
+          int sum = currSum + dist.length - subtreeSize[child] - subtreeSize[child];
+          dfs2(child, node, sum, graph, dist, subtreeSize);
         }
     }
 }
